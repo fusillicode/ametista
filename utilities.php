6e0785ec9790330c35006259533d769287737487
gr4devel <gr.4devel@gmail.com>
2013-10-02 17:22:17 +0200

    change coding style for variables in utilities.php

--- utilities.php
+++ utilities.php
@@ -1,50 +1,49 @@
 <?php
 
-function getFilesAlternative($rootDirectory)
+function getFilesAlternative($root_directory)
 {
   $files = array();
-  $stack[] = $rootDirectory;
+  $stack[] = $root_directory;
   while ($stack) {
-    $currentDirectory = array_pop($stack);
-    $handle = opendir($currentDirectory);
+    $current_directory = array_pop($stack);
+    $handle = opendir($current_directory);
     while ($content = readdir($handle)) {
       if ($content === '.' || $content === '..') continue;
-      $currentElement = "{$currentDirectory}/{$content}";
-      $extension = pathinfo($currentElement, PATHINFO_EXTENSION);
-      if (is_file($currentElement) && is_readable($currentElement) && $extension === 'php')
-        $files[] = $currentElement;
-      elseif (is_dir($currentElement))
-        $stack[] = $currentElement;
+      $current_element = "{$current_directory}/{$content}";
+      $extension = pathinfo($current_element, PATHINFO_EXTENSION);
+      if (is_file($current_element) && is_readable($current_element) && $extension === 'php')
+        $files[] = $current_element;
+      elseif (is_dir($current_element))
+        $stack[] = $current_element;
     }
     closedir($handle);
   }
   return $files;
 }
 
-function getFiles($rootDirectory)
+function getFiles($root_directory)
 {
   $files = array();
-  $stack[] = $rootDirectory;
+  $stack[] = $root_directory;
   while ($stack) {
-    $currentDirectory = array_pop($stack);
-    $directoryContent = scandir($currentDirectory);
-    foreach ($directoryContent as $content) {
-      if ($content === '.' || $content === '..')
-        continue;
-      $currentElement = "{$currentDirectory}/{$content}";
-      $extension = pathinfo($currentElement, PATHINFO_EXTENSION);
-      if (is_file($currentElement) && is_readable($currentElement) && $extension === 'php')
-        $files[] = $currentElement;
-      elseif (is_dir($currentElement))
-        $stack[] = $currentElement;
+    $current_directory = array_pop($stack);
+    $directory_content = scandir($current_directory);
+    foreach ($directory_content as $content) {
+      if ($content === '.' || $content === '..') continue;
+      $current_element = "{$current_directory}/{$content}";
+      $extension = pathinfo($current_element, PATHINFO_EXTENSION);
+      if (is_file($current_element) && is_readable($current_element) && $extension === 'php')
+        $files[] = $current_element;
+      elseif (is_dir($current_element))
+        $stack[] = $current_element;
     }
   }
   return $files;
 }
 
-function printJudyObject(Judy $judyObject, $level = 0)
+function printJudyObject(Judy $judy_object, $level = 0)
 {
-  foreach ($judyObject as $key => $value) {
+  foreach ($judy_object as $key => $value) {
     echo "level {$level} {$key}\n";
     if ($value instanceof Judy)
       printJudyObject($value, $level+1);
@@ -62,10 +61,10 @@ function checkJudyExtension()
   return true;
 }
 
-function replaceExtension($fileName, $newExtension)
+function replaceExtension($file_name, $new_extension)
 {
-  $pathInformation = pathinfo($fileName);
-  return "{$pathInformation['filename']}.{$newExtension}";
+  $path_information = pathinfo($file_name);
+  return "{$path_information['file_name']}.{$new_extension}";
 }
 
 ?>
