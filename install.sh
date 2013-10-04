4a7e3ee6e52695a8a9cb37fdc72d8fb5c1e852ed
gr4devel <gr.4devel@gmail.com>
2013-10-04 11:59:05 +0200

    fix initial clean up in installation process

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 php -r "eval('?>'.file_get_contents('https://getcomposer.org/installer'));"
 
 # Remove already present vendor directory to cleanly install Predis package
-rm vendor
+rm -rf vendor
 
 # Update Composer to install/update PHP-Parser and Predis packages
 composer update
@@ -14,3 +14,4 @@ wget http://download.redis.io/releases/redis-2.6.16.tar.gz
 tar xzf redis-2.6.16.tar.gz
 cd redis-2.6.16
 make
+make test
--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInit0b3076b93ca57ad8ca2316aee111048a::getLoader();
+return ComposerAutoloaderInit06f11bf0aca639bcb2aebac826738952::getLoader();
