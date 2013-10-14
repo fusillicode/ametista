38054e1d6d33efd38e364d5afc2d07141bfc074b
gr4devel <gr.4devel@gmail.com>
2013-10-14 09:34:29 +0200

    add test classes and statements

--- test_codebase/1.php
+++ test_codebase/1.php
@@ -4,6 +4,29 @@ namespace Ciao;
 
 $a = "ciao" + "1.2";
 $a = 'ciao1' + 1;
-$a = 'ciao1' + array(1);
-$a = 1 + array(1);
-$a = "1" + array(1);
+// $a = 'ciao1' + array(1);
+// $a = 1 + array(1);
+// $a = "1" + array(1);
+
+interface Pape
+{
+	public function leggi();
+}
+
+class topo
+{
+
+}
+
+class Pluto extends topo implements Pape
+{
+	public function leggi ()
+	{
+		echo 'ciao';
+	}
+}
+
+
+$a = new Pluto();
+$b = new topo();
+var_dump(get_class($a), get_class($b));
