5723825b63fe305cb9859c9bbc4aaa4c41abf61e
gr4devel <gr.4devel@gmail.com>
2013-10-15 10:01:07 +0200

    try to make a little bit order

--- Model.php
+++ Model.php
@@ -46,33 +46,33 @@ class Model
   private function insertNode(PHPParser_Node $node_object)
   {
     switch (get_class($node_object)) {
-      // case 'PHPParser_Node_Stmt_Namespace':
-      //   $this->insertNamespace($node_object);
-      //   break;
+      case 'PHPParser_Node_Stmt_Namespace':
+        $this->insertNamespace($node_object);
+        break;
       case 'PHPParser_Node_Stmt_Class':
         $this->insertClass($node_object);
         break;
       case 'PHPParser_Node_Stmt_Function':
         $this->insertFunction($node_object);
         break;
-      // case 'PHPParser_Node_Stmt_ClassMethod':
-      //   $this->insertClassMethod($node_object);
-      //   break;
-      // case 'PHPParser_Node_Expr_Assign':
-      //   $this->insertAssignement($node_object);
-      //   break;
+      case 'PHPParser_Node_Stmt_ClassMethod':
+        $this->insertClassMethod($node_object);
+        break;
+      case 'PHPParser_Node_Expr_Assign':
+        $this->insertAssignement($node_object);
+        break;
     }
   }
 
-  // private function insertNamespace(PHPParser_Node_Stmt_Namespace $node_object)
-  // {
-  //   foreach ($node_object->name->parts as $key => $sub_namespace_name) {
-  //     $parent_namespace = $this->_current_namespace;
-  //     $this->_current_namespace .= "\\{$sub_namespace_name}";
-  //     $this->_redis->sadd("{$parent_namespace}:[N", $this->_current_namespace);
-  //     $this->_redis->sadd("{$this->_current_namespace}:]N", $parent_namespace);
-  //   }
-  // }
+  private function insertNamespace(PHPParser_Node_Stmt_Namespace $node_object)
+  {
+    foreach ($node_object->name->parts as $key => $sub_namespace_name) {
+      $parent_namespace = $this->_current_namespace;
+      $this->_current_namespace .= "\\{$sub_namespace_name}";
+      $this->_redis->sadd("{$parent_namespace}:[N", $this->_current_namespace);
+      $this->_redis->sadd("{$this->_current_namespace}:]N", $parent_namespace);
+    }
+  }
 
   private function insertClass(PHPParser_Node_Stmt_Class $node_object)
   {
@@ -97,17 +97,17 @@ class Model
     $this->_redis->sadd("{$superclass_key}<", "{$this->_current_class}");
   }
 
-  private function updateInsertedClassKeys($class_name, $class_key)
-  {
-    if (!$class_references = $this->_redis->keys($class_name)) return false;
-    foreach ($class_references as $key => $class_reference) {
-      $this->_redis->sadd(str_replace($class_reference,
-                                      $class_key,
-                                      $class_reference),
-                          $this->_redis->smembers($class_reference));
-      $this->_redis->del($class_reference);
-    }
-  }
+  // private function updateInsertedClassKeys($class_name, $class_key)
+  // {
+  //   if (!$class_references = $this->_redis->keys($class_name)) return false;
+  //   foreach ($class_references as $key => $class_reference) {
+  //     $this->_redis->sadd(str_replace($class_reference,
+  //                                     $class_key,
+  //                                     $class_reference),
+  //                         $this->_redis->smembers($class_reference));
+  //     $this->_redis->del($class_reference);
+  //   }
+  // }
 
   private function insertFunction(PHPParser_Node_Stmt_Function $node_object)
   {
@@ -117,25 +117,25 @@ class Model
 
   private function insertClassMethod(PHPParser_Node_Stmt_ClassMethod $node_object)
   {
-    $this->insert("classMethod {$node_object->name}");
+
   }
 
   private function insertAssignement(PHPParser_Node_Expr_Assign $node_object)
   {
-    if ($node_object->var instanceof PHPParser_Node_Expr_Variable)
-      $this->insertVariable($node_object->var);
-    elseif ($node_object->var->name instanceof PHPParser_Node_Expr_Variable)
-      $this->insertVariable($node_object->var->name);
-    elseif ($node_object->var->name instanceof String)
-      $this->insert("variable {$node_object->var->name}");
+    // if ($node_object->var instanceof PHPParser_Node_Expr_Variable)
+    //   $this->insertVariable($node_object->var);
+    // elseif ($node_object->var->name instanceof PHPParser_Node_Expr_Variable)
+    //   $this->insertVariable($node_object->var->name);
+    // elseif ($node_object->var->name instanceof String)
+    //   $this->insert("variable {$node_object->var->name}");
   }
 
   private function insertVariable(PHPParser_Node_Expr_Variable $node_object)
   {
-    if ($node_object->name instanceof PHPParser_Node_Expr_Variable)
-      $this->insertVariable($node_object->name);
-    else
-      $this->insert("variable {$node_object->name}");
+    // if ($node_object->name instanceof PHPParser_Node_Expr_Variable)
+    //   $this->insertVariable($node_object->name);
+    // else
+    //   $this->insert("variable {$node_object->name}");
   }
 
 }
