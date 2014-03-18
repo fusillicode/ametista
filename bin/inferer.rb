693cfbef3ddf30fb361689a619198be82df9222c
fusillicode <fusillicode@gmail.com>
2014-03-18 11:19:21 +0100

    maybe...

--- bin/inferer.rb
+++ bin/inferer.rb
@@ -2,8 +2,31 @@
 
 require "redis"
 require "nokogiri"
+require "ohm"
 
-redis = Redis.new
+class Event < Ohm::Model
+  attribute :name
+  reference :venue, :Venue
+  set :participants, :Person
+  counter :votes
 
-xml = Nokogiri.parse redis.get './test_codebase/classes/log/AbstractLogger.php'
-p xml.xpath('//node:*').first
+  index :name
+end
+
+class Venue < Ohm::Model
+  attribute :name
+  collection :events, :Event
+end
+
+class Person < Ohm::Model
+  attribute :name
+end
+
+Ohm.connect :url => "redis://127.0.0.1:6379"
+
+# xml = Nokogiri.parse redis.get './test_codebase/classes/log/AbstractLogger.php'
+# xml.xpath('//node:*').first
+
+event = Event.create :name => "Ohm Worldwide Conference 2031"
+event = Event[3]
+p Event.all
