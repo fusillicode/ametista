1db663b7f64696d61a003ba41e1afb9b29d0e01d
fusillicode <fusillicode@gmail.com>
2014-03-20 13:45:52 +0100

    try with css selectors

--- bin/inferer.rb
+++ bin/inferer.rb
@@ -51,10 +51,33 @@ redis = Redis.new
 xml = Nokogiri::XML redis.get './test_codebase/controllers/front/1.php'
 
 # Classes
-xml.xpath('.//node:Stmt_Class').each do |classInXML|
+xml.css("node|Stmt_Class").each do |classInXML|
 
   # Class name
-  puts 'C --- ' + classInXML.xpath('.//subNode:namespacedName//scalar:string').text
+  p 'C --- ' + classInXML.css("> subNode|namespacedName subNode|parts scalar|string").text
+
+  # Class methods
+  classInXML.css("node|Stmt_ClassMethod").each do |method|
+
+    # Class method name
+    p 'M --- ' + method.css("> subNode|name scalar|string").text
+
+    # Class method args
+    method.css("> subNode|params node|Param").each do |param|
+      p 'A --- ' + param.css("> subNode|type > node|Name_FullyQualified > subNode|parts scalar|string:first-child").text + '/' + param.css("> subNode|name > scalar|string").text
+    end
+
+  end
+
+end
+
+exit
+
+# Classes
+xml.css("node|Stmt_Class").each do |classInXML|
+
+  # Class name
+  puts 'C --- ' + classInXML.css("subNode|namespacedName scalar|string").text
 
   # Class methods
   classInXML.xpath('.//node:Stmt_ClassMethod').each do |method|
@@ -68,7 +91,21 @@ xml.xpath('.//node:Stmt_Class').each do |classInXML|
     end
 
     # Class properties
-    puts method.xpath(".//node:Expr_Assign/subNode:var/node:Expr_PropertyFetch/subNode:var[1]//subNode:name/scalar:string[. = 'this']")
+    # method.xpath(".//node:Expr_Assign/subNode:var/node:Expr_PropertyFetch/subNode:var[1]//subNode:name/scalar:string[. = 'this']").each do |property|
+    #   p property
+    # end
+
+    method.xpath(".//node:Expr_Assign/subNode:var//parent::subNode:var").each do |nodeExpression|
+      p nodeExpression
+      # p lhs.xpath(".//*[self::subNode:name/scalar:string or self::subNode:dim/subNode:value/*[self::scalar:string or self::scalar:int]]").text
+    end
+
+    # devo prendere prima tutti gli Expr_PropertyFetch e gli Expr_ArrayDimFetch all'interno di ciascun singolo Expr_Assign.
+    # ogni Expr_PropertyFetch o Expr_ArrayDimFetch ha un subNode:var e un subNode:name.
+    # il subNode:var può essere a sua volta un Expr_PropertyFetch o un Expr_ArrayDimFetch mentre il subNode:name è quello che è
+    # posto alla destra del Expr_PropertyFetch o del Expr_ArrayDimFetch a seconda dei casi
+
+
     # puts method.xpath(".//node:Expr_Assign/subNode:var/node:Expr_ArrayDimFetch/subNode:var[1]//subNode:name/scalar:string[. = 'this']")
 
   end
