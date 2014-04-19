e145db7cf7fcc1b174a4824557d9af73482b442a
fusillicode <fusillicode@gmail.com>
2014-04-19 11:27:19 +0200

    damend global namespace

--- bin/inferer.rb
+++ bin/inferer.rb
@@ -26,26 +26,50 @@ class INamespace < Ohm::Model
   reference :parent_i_namespace, :INamespace
   reference :statements, :IRawContent
 
-  collection :i_classes, :IClass, :i_namespace
   collection :i_functions, :IFunction, :i_namespace
+  collection :i_classes, :IClass, :i_namespace
 
   class << self
 
-    attr_accessor :namespace
+    attr_accessor :ast
     attr_accessor :model
+    attr_accessor :namespace
 
-    def build(namespace, model)
-      self.namespace = namespace
+    def build(ast, model)
+      self.ast = ast
       self.model = model
       build_global_namespace
-      build_subnamespaces
-      build_raw_content
-      build_assignements
+      build_namespaces
     end
 
     def build_global_namespace
       model.current_i_namespace = self.create(:unique_name => '\\',
                                               :name => '\\')
+      self.namespace = ast
+      build_raw_content
+      build_assignements
+      build_functions
+      # build_classes
+    end
+
+    def build_namespaces
+      get_namespaces.each do |namespace|
+        self.namespace = namespace
+        build_namespace
+        set_global_namespace
+      end
+    end
+
+    def build_namespace
+      build_subnamespaces
+      build_raw_content
+      build_assignements
+      build_functions
+      # build_classes
+    end
+
+    def set_global_namespace
+      model.current_i_namespace = self.with(:unique_name => '\\')
     end
 
     def build_subnamespaces
@@ -56,28 +80,42 @@ class INamespace < Ohm::Model
       end
     end
 
-    def build_assignements
-      get_assignements.each do |assignement|
-        # puts model.get_LHS assignement
-      end
-    end
-
     def build_raw_content
       model.current_i_namespace.statements = IRawContent.create(:content => get_raw_content,
                                                                 :i_namespace => model.current_i_namespace)
       model.current_i_namespace.save
     end
 
+    def build_assignements
+      get_assignements.each do |assignement|
+        # puts model.get_LHS assignement
+      end
+    end
+
+    def build_functions
+      get_functions.each do |function|
+        IFunction.build(function, model)
+      end
+    end
+
+    def get_namespaces
+      ast.xpath('.//node:Stmt_Namespace')
+    end
+
     def get_subnamespaces
       namespace.xpath('./subNode:name/node:Name/subNode:parts/scalar:array/scalar:string')
+    end
+
+    def get_raw_content
+      namespace.xpath('./subNode:stmts/scalar:array/*[name() != "node:Stmt_Function" and name() != "node:Stmt_Class"]')
     end
 
     def get_assignements
       namespace.xpath('./subNode:stmts/scalar:array/node:Expr_Assign/subNode:var')
     end
 
-    def get_raw_content
-      namespace.xpath('./subNode:stmts/scalar:array/*[name() != "node:Stmt_Function" and name() != "node:Stmt_Class"]')
+    def get_functions
+      namespace.xpath('./subNode:stmts/scalar:array/node:Stmt_Function')
     end
 
   end
@@ -315,7 +353,7 @@ class Model
 
   def build
     while ast = parse(asts)
-      build_namespaces(ast)
+      INamespace.build(ast, self)
     end
   end
 
@@ -329,27 +367,6 @@ class Model
 
   def last_ast?(ast)
     ast == "THAT'S ALL FOLKS!"
-  end
-
-  def build_namespaces(ast)
-    namespaces(ast).each do |namespace|
-      INamespace.build(namespace, self)
-      build_functions(namespace)
-    end
-  end
-
-  def build_functions(namespace)
-    functions(namespace).each do |function|
-      IFunction.build(function, self)
-    end
-  end
-
-  def namespaces(ast)
-    ast.xpath('.//node:Stmt_Namespace')
-  end
-
-  def functions(namespace)
-    namespace.xpath('./subNode:stmts/scalar:array/node:Stmt_Function')
   end
 
   # ##############################################################################
@@ -427,14 +444,14 @@ IFunction.all.to_a.each do |function|
   puts function.return_statements
 end
 
-# INamespace.all.to_a.each do |namespace|
-#   # puts namespace.name
-#   # puts 'with parent ' + (namespace.parent_i_namespace ? namespace.parent_i_namespace.name : '')
-#   # puts namespace.statements
-#   # p namespace.i_functions
-#   # p namespace.i_functions.count
-#   # p namespace.statements.content unless namespace.statements.nil?
-# end
+INamespace.all.to_a.each do |namespace|
+  puts namespace.name
+  # puts 'with parent ' + (namespace.parent_i_namespace ? namespace.parent_i_namespace.name : '')
+  # puts namespace.statements
+  # p namespace.i_functions
+  # p namespace.i_functions.count
+  # p namespace.statements.content unless namespace.statements.nil?
+end
 
 exit
 
