ddfcd54c2d7f4181051d919c2bff5ab18801957d
fusillicode <fusillicode@gmail.com>
2014-04-25 18:56:15 +0200

    remove attr_accessors

--- bin/INamespace.rb
+++ bin/INamespace.rb
@@ -21,21 +21,17 @@ class INamespace < Ohm::Model
 
   class << self
 
-    attr_accessor :ast
-    attr_accessor :model
-    attr_accessor :namespace
-
     def build(ast, model)
-      self.ast = ast
-      self.model = model
+      @ast = ast
+      @model = model
       build_global_namespace
       build_other_namespaces
     end
 
     def build_global_namespace
-      model.current_i_namespace = self.create(:unique_name => '\\',
-                                              :name => '\\')
-      self.namespace = ast
+      @model.current_i_namespace = self.create(:unique_name => '\\',
+                                               :name => '\\')
+      @namespace = @ast
       build_raw_content
       # build_assignements
       build_functions
@@ -44,7 +40,7 @@ class INamespace < Ohm::Model
 
     def build_other_namespaces
       get_namespaces.each do |namespace|
-        self.namespace = namespace
+        @namespace = namespace
         build_namespace
         set_global_namespace
       end
@@ -59,26 +55,26 @@ class INamespace < Ohm::Model
     end
 
     def set_global_namespace
-      model.current_i_namespace = self.with(:unique_name, '\\')
+      @model.current_i_namespace = self.with(:unique_name, '\\')
     end
 
     def build_subnamespaces
       get_subnamespaces.each do |subnamespace|
-        model.current_i_namespace = self.create(:unique_name => get_subnamespace_unique_name(subnamespace),
-                                                :name => subnamespace.text,
-                                                :parent_i_namespace => model.current_i_namespace)
+        @model.current_i_namespace = self.create(:unique_name => get_subnamespace_unique_name(subnamespace),
+                                                 :name => subnamespace.text,
+                                                 :parent_i_namespace => @model.current_i_namespace)
       end
     end
 
     def build_raw_content
-      model.current_i_namespace.statements = IRawContent.create(:content => get_raw_content,
-                                                                :i_namespace => model.current_i_namespace)
-      model.current_i_namespace.save
+      @model.current_i_namespace.statements = IRawContent.create(:content => get_raw_content,
+                                                                 :i_namespace => @model.current_i_namespace)
+      @model.current_i_namespace.save
     end
 
     # def build_assignements
     #   get_assignements.each do |assignement|
-    #     # puts model.get_LHS assignement
+    #     # puts @model.get_LHS assignement
     #   end
     # end
 
@@ -95,31 +91,31 @@ class INamespace < Ohm::Model
     end
 
     def get_namespaces
-      ast.xpath('.//node:Stmt_Namespace')
+      @ast.xpath('.//node:Stmt_Namespace')
     end
 
     def get_subnamespaces
-      namespace.xpath('./subNode:name/node:Name/subNode:parts/scalar:array/scalar:string')
+      @namespace.xpath('./subNode:name/node:Name/subNode:parts/scalar:array/scalar:string')
     end
 
     def get_subnamespace_unique_name(subnamespace)
-      "#{model.current_i_namespace.unique_name}\\#{subnamespace.text}"
+      "#{@model.current_i_namespace.unique_name}\\#{subnamespace.text}"
     end
 
     def get_raw_content
-      namespace.xpath('./subNode:stmts/scalar:array/*[name() != "node:Stmt_Function" and name() != "node:Stmt_Class"]')
+      @namespace.xpath('./subNode:stmts/scalar:array/*[name() != "node:Stmt_Function" and name() != "node:Stmt_Class"]')
     end
 
     def get_assignements
-      namespace.xpath('./subNode:stmts/scalar:array/node:Expr_Assign/subNode:var')
+      @namespace.xpath('./subNode:stmts/scalar:array/node:Expr_Assign/subNode:var')
     end
 
     def get_functions
-      namespace.xpath('./subNode:stmts/scalar:array/node:Stmt_Function')
+      @namespace.xpath('./subNode:stmts/scalar:array/node:Stmt_Function')
     end
 
     def get_classes
-      namespace.xpath('./subNode:stmts/scalar:array/node:Stmt_Class')
+      @namespace.xpath('./subNode:stmts/scalar:array/node:Stmt_Class')
     end
 
   end
