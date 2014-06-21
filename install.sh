357b6a1b7a9200feb4a73d77a028c90d8c34e1c4
fusillicode <fusillicode@gmail.com>
2014-06-21 12:42:28 +0200

    parametrized install.sh and add MongoDB installation

--- install.sh
+++ install.sh
@@ -10,10 +10,36 @@ php composer.phar install
 php composer.phar update
 
 # Install Redis in vendor subdirectory
+REDIS_VERSION="2.8.5"
+REDIS="redis-$REDIS_VERSION"
+REDIS_PACKAGE="$REDIS.tar.gz"
 cd vendor
-wget http://download.redis.io/releases/redis-2.8.5.tar.gz
-tar xzf redis-2.8.5.tar.gz
-cd redis-2.8.5
+wget "http://download.redis.io/releases/$REDIS_PACKAGE"
+tar xzf "$REDIS_PACKAGE"
+cd "$REDIS"
 make
+cd ../..
 
+# Install MongoDb in vendor subdirectory
+cd vendor
+case `uname -s` in
+  Linux*)
+    PLATFORM="linux"
+    ;;
+  Darwin*)
+    PLATFORM="osx"
+    ;;
+  *)
+    echo "unknown: $OSTYPE"
+    exit 1
+    ;;
+esac
+MONGODB_VERSION="2.6.3"
+MONGODB="mongodb-$PLATFORM-x86_64-$MONGODB_VERSION"
+MONGODB_PACKAGE="$MONGODB.tgz"
+curl -O "http://downloads.mongodb.org/$PLATFORM/$MONGODB_PACKAGE"
+tar -zxvf "$MONGODB_PACKAGE"
+
+# Install required Gems
 bundle install
+
