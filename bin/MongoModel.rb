b78381d2605eefddebc046dc3a81143451e4968f
fusillicode <fusillicode@gmail.com>
2014-06-28 10:57:57 +0200

    add some things...

--- bin/MongoModel.rb
+++ bin/MongoModel.rb
@@ -2,30 +2,35 @@ require "mongoid"
 
 class IScope
   include Mongoid::Document
-  belongs_to :parent_scope, class_name: 'IScope', inverse_of: :child_scopes
-  has_many :child_scopes, class_name: 'IScope', inverse_of: :parent_scope
   has_many :assignements, class_name: 'IAssignement', inverse_of: :scope
-  field :id, type: String
+  has_many :branches, class_name: 'IBranch', inverse_of: :scope
   field :name, type: String
-  field :statements, type: String
+end
+
+class IBranch < IScope
+  belongs_to :scope, class_name: 'IScope', inverse_of: :branches
 end
 
 class IAssignement
   include Mongoid::Document
   has_one :variable, class_name: 'IVariable', inverse_of: :assignements
   belongs_to :scope, class_name: 'IScope', inverse_of: :assignements
-  field :value, type: String
+  field :RHS, type: String
 end
 
 class IVariable
   include Mongoid::Document
   has_many :assignements, class_name: 'IAssignement', inverse_of: :variable
+  has_and_belongs_to_many :types, class_name: 'IType', inverse_of: :variables
+end
+
+class IType
+  has_and_belongs_to_many :variables, class_name: 'IVariable', inverse_of: :types
   field :name, type: String
 end
 
 class IProcedure < IScope
   has_many :parameters, class_name: 'IParameter', inverse_of: :procedure
-  # has_many :local_variables, class_name: 'ILocalVariable', inverse_of: :procedure
   field :return_value, type: String
 end
 
@@ -34,11 +39,10 @@ class INamespace < IScope
   has_many :child_namespaces, class_name: 'INamespace', inverse_of: :parent_namespace
   has_many :functions, class_name: 'IFunction', inverse_of: :namespace
   has_many :classes, class_name: 'IClass', inverse_of: :namespace
-  # has_many :global_variables, class_name: 'IGlobalVariable', inverse_of: :namespace
 end
 
-class IClass < IScope
-  has_one :parent_class, class_name: 'IClass', inverse_of: :child_classes
+class IClass
+  belongs_to :parent_class, class_name: 'IClass', inverse_of: :child_classes
   has_many :child_classes, class_name: 'IClass', inverse_of: :parent_class
   belongs_to :namespace, class_name: 'INamespace', inverse_of: :classes
   has_many :methods, class_name: 'IMethod', inverse_of: :class
@@ -93,19 +97,61 @@ class MongoDaemon
 
 end
 
+class Model
+
+  def initialize
+    @global_variables = ['GLOBALS', '_POST', '_GET', '_REQUEST', '_SERVER',
+                         'FILES', '_SESSION', '_ENV', '_COOKIE']
+    @types = ['bool', 'int', 'double', 'string', 'array', 'null']
+    @magic_constants = ['Scalar_LineConst', 'Scalar_FileConst',
+                        'Scalar_DirConst', 'Scalar_FuncConst',
+                        'Scalar_ClassConst', 'Scalar_TraitConst',
+                        'Scalar_MethodConst', 'Scalar_NSConst']
+  end
+
+end
+
+require 'redis'
+require 'nokogiri'
+
 class ModelBuilder
 
-  def initialize mongoid_configuration = './mongoid.yml', environment = :development
+  def initialize mongoid_configuration = './mongoid.yml', environment = :development, redis = nil, model = nil
     @mongoid_configuration = mongoid_configuration
     @environment = environment
     connect
+    @redis = redis or Redis.new
+    @model = model or Model.new
   end
 
   def connect
     Mongoid.load!(@mongoid_configuration, @environment)
   end
 
-  def initialize_global_variables
+  def build
+    while ast = parse(asts)
+      INamespace.build(ast, @model)
+    end
+  end
+
+  def asts
+    redis.brpoplpush('xmls_asts', 'done', :timeout => 0)
+  end
+
+  def parse ast
+    Nokogiri::XML(ast) unless last_ast?(ast)
+  end
+
+  def last_ast? ast
+    ast == "THAT'S ALL FOLKS!"
+  end
+
+  def build_global_namespace
+
+  end
+
+  def build_global_variables
+
   end
 
 end
