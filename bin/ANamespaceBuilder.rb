657a12b3295eb23589c5f4fa14603e9be1cdb7bc
fusillicode <fusillicode@gmail.com>
2014-08-12 17:47:11 +0200

    fix namespace hierarchy?

--- bin/ANamespaceBuilder.rb
+++ bin/ANamespaceBuilder.rb
@@ -1,113 +1,36 @@
-class XMLParser
+class ANamespaceAstQuerier
 
-  def parse ast
-    Nokogiri::XML(ast)
-  end
-
-end
-
-class ANamespaceParser
-
-  extend Initializer
-  initialize_with ({
-    parser: XMLParser.new
-  })
-
-end
-
-class ANamespaceBuilder
-
-  extend Initializer
-  initialize_with ({
-    ast: nil,
-    parser: ANamespaceParser.new
-  })
-
-  def build
-    build_global_namespace
-    # build_other_namespaces
-  end
-
-  def build_global_namespace
-    global_namespace = ANamespace.find_or_create_by(id: '\\', name: '\\')
-    global_namespace.subnamespaces = build_subnamespaces
-    global_namespace.functions = build_functions
-    global_namespace.classes = build_classes
-  end
-
-  def build_other_namespaces
-    get_namespaces.each do |namespace|
-      ast = namespace
-      build_subnamespaces
-      build_namespace
-    end
-  end
-
-  def build_namespace
-    # build_global_variables
-    # build_functions
-    # build_classes
-  end
-
-  def build_subnamespaces
-    get_subnamespaces.map do |subnamespace|
-      ANamespace.find_or_create_by(unique_name: get_subnamespace_unique_name(subnamespace), name: subnamespace.text)
-    end
-  end
-
-  def build_global_variables
-      get_assignements.each do |global_variable|
-        p get_variable_name(global_variable)
-      end
-      # get_global_variables.each do |global_variable|
-      # global_variable_name = get_variable_name(get_global_variable_value)
-      # AVariable.create(:unique_name => global_variable_name,
-      #                  :name => global_variable_name,
-      #                  :type => 'global',
-      #                  :value => get_global_variable_value(get_global_variable_value),
-      #                  :i_procedure => model.send("current_#{procedure_type}"))
-      # end
-  end
-
-  def build_functions
-    get_functions.each do |function|
-      model.ast = function
-      builded_entity[:model].functions << AFunctionBuilder.build(model)
-    end
-  end
-
-  def build_classes
-    get_classes.each do |a_class|
-      AClassBuilder.build(model)
-    end
-  end
-
-  def get_namespaces
+  def namespaces(ast)
     ast.xpath('.//node:Stmt_Namespace')
   end
 
-  def get_subnamespaces
+  def inline_namespaces(ast)
     ast.xpath('./subNode:name/node:Name/subNode:parts/scalar:array/scalar:string')
   end
 
-  def get_subnamespace_unique_name(subnamespace)
-    "#{builded_entity[:model].unique_name}\\#{subnamespace.text}"
+  def namespace_name(ast)
+    ast.xpath('./subNode:name/node:Name/subNode:parts/scalar:array/scalar:string[last()]').text
   end
 
-  def get_statements
+  def namespace_unique_name(ast)
+    subnamespaces = ast.xpath('./subNode:name/node:Name/subNode:parts/scalar:array/scalar:string')
+    subnamespaces.map{ |subnamespace| "\\#{subnamespace.text}" }.join
+  end
+
+  def statements(ast)
     ast.xpath('./subNode:stmts/scalar:array/*[name() != "node:Stmt_Function" and name() != "node:Stmt_Class"]')
   end
 
   # le variabili assegnate
-  def get_assignements
+  def assignements(ast)
     ast.xpath('./subNode:stmts/scalar:array/node:Expr_Assign/subNode:var')
   end
 
-  def get_global_variable_value(global_variable)
+  def global_variable_value(global_variable)
     global_variable.xpath('./subNode:expr')
   end
 
-  def get_variable_name(node)
+  def variable_name(node)
 
     node = node.xpath('./*[1]')[0]
 
@@ -121,12 +44,12 @@ class ANamespaceBuilder
       name
     when 'Expr_PropertyFetch'
       # non tratto proprietà con nomi dinamici
-      return false if !var = get_variable_name(node.xpath('./subNode:var'))
-      return false if !name = get_variable_name(node.xpath('./subNode:name'))
+      return false if !var = variable_name(node.xpath('./subNode:var'))
+      return false if !name = variable_name(node.xpath('./subNode:name'))
       var << '->' << name
     when 'Expr_ArrayDimFetch'
       # non tratto indici di array dinamici
-      return var if !var = get_variable_name(node.xpath('./subNode:var'))
+      return var if !var = variable_name(node.xpath('./subNode:var'))
       dim = node.xpath('./subNode:dim/*[name() = "node:Scalar_String" or name() = "node:Scalar_LNumber"]/subNode:value/*').text
       return false if dim.nil? or dim.empty?
       var << '[' << dim << ']'
@@ -138,12 +61,109 @@ class ANamespaceBuilder
 
   end
 
-  def get_functions
+  def functions(ast)
     ast.xpath('./subNode:stmts/scalar:array/node:Stmt_Function')
   end
 
-  def get_classes
+  def classes(ast)
     ast.xpath('./subNode:stmts/scalar:array/node:Stmt_Class')
+  end
+
+end
+
+class ANamespaceBuilder
+
+  extend Initializer
+  initialize_with ({
+    ast: nil,
+    querier: ANamespaceAstQuerier.new
+  })
+
+  def build ast = nil
+    @ast ||= ast
+    build_global_namespace
+  end
+
+  def build_global_namespace
+    global_namespace = ANamespace.find_or_create_by(
+      unique_name: '\\',
+      name:        '\\'
+    )
+    global_namespace.functions = build_functions
+    global_namespace.classes = build_classes
+    global_namespace.subnamespaces = build_namespaces
+  end
+
+  def build_namespaces
+    querier.namespaces(ast).map do |namespace_ast|
+      @ast = namespace_ast
+      build_namespace
+    end
+  end
+
+  def build_namespace
+    inline_namespaces = build_inline_namespaces
+    inline_namespaces.last.functions = build_functions
+    inline_namespaces.last.classes = build_classes
+    inline_namespaces.first
+  end
+
+  def build_inline_namespaces
+    unique_name = ''
+    inline_namespaces_ast = querier.inline_namespaces(ast)
+    if inline_namespaces_ast.count == 1
+      name = inline_namespaces_ast.first.text
+      unique_name = "#{unique_name}\\#{name}"
+      return [ ANamespace.find_or_create_by(
+                unique_name: unique_name,
+                name: name
+              ) ]
+    end
+    parent_unique_name = ''
+    inline_namespaces = inline_namespaces_ast.each_cons(2).map do |namespaces|
+      parent_name = namespaces.first.text
+      parent_unique_name = "#{parent_unique_name}\\#{parent_name}"
+      child_name = namespaces.second.text
+      child_unique_name = "#{parent_unique_name}\\#{child_name}"
+      ANamespace.find_or_create_by(
+        unique_name: parent_unique_name,
+        name: parent_name
+      ).subnamespaces.find_or_create_by(
+        unique_name: child_unique_name,
+        name: child_name
+      )
+    end
+    inline_namespaces
+  end
+
+  def build_global_variables
+      querier.assignements.each do |global_variable|
+        p querier.variable_name(global_variable)
+      end
+      # global_variables.each do |global_variable|
+      # global_variable_name = variable_name(global_variable_value)
+      # AVariable.create(:unique_name => global_variable_name,
+      #                  :name => global_variable_name,
+      #                  :type => 'global',
+      #                  :value => global_variable_value(global_variable_value),
+      #                  :i_procedure => model.send("current_#{procedure_type}"))
+      # end
+  end
+
+  def build_functions
+    querier.functions(ast).map do |function_ast|
+      AFunctionBuilder.build(function_ast)
+    end
+  end
+
+  def build_classes
+    querier.classes(ast).map do |class_ast|
+      AClassBuilder.build(class_ast)
+    end
+  end
+
+  def subnamespace_unique_name(subnamespace)
+    "#{builded_entity[:model].unique_name}\\#{subnamespace.text}"
   end
 
 end
