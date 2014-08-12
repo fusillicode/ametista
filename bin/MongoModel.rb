c0b9ef7c6fc2ad7dd40024f69d8c7a0f3b02bb0a
fusillicode <fusillicode@gmail.com>
2014-08-12 10:41:23 +0200

    instance variables

--- bin/MongoModel.rb
+++ bin/MongoModel.rb
@@ -156,11 +156,11 @@ class MongoDaemon
   })
 
   def start
-    pid = Process.spawn "#{path} --fork --dbpath #{database} --logpath #{log} > /dev/null"
+    @pid = Process.spawn "#{@path} --fork --dbpath #{@database} --logpath #{@log} > /dev/null"
   end
 
   def stop
-    Process.kill('TERM', pid) unless pid
+    Process.kill('TERM', @pid) unless @pid
   end
 
 end
@@ -181,11 +181,11 @@ class RedisDataSource
   })
 
   def read
-    return data = redis.brpoplpush(channel, 'done', timeout: timeout) until end_of_data?
+    return @data = redis.brpoplpush(@channel, 'done', timeout: @timeout) until end_of_data?
   end
 
   def end_of_data?
-    data == last_data
+    @data == @last_data
   end
 
 end
@@ -203,15 +203,23 @@ class LanguageBuilder
   end
 
   def build_types
-    language.types.each do |type|
+    @language.types.each do |type|
       AType.create(name: type)
     end
   end
 
   def build_superglobals
-    language.superglobals.each do |superglobal|
+    @language.superglobals.each do |superglobal|
       AGlobalVariable.create(:unique_name => superglobal)
     end
+  end
+
+end
+
+class XMLParser
+
+  def parse ast
+    Nokogiri::XML(ast)
   end
 
 end
@@ -221,17 +229,23 @@ class ModelBuilder
   extend Initializer
   initialize_with ({
     data_source: RedisDataSource.new,
-    language_builder: LanguageBuilder.new
+    language_builder: LanguageBuilder.new,
+    top_level_builder: ANamespaceBuilder.new,
+    parser: XMLParser.new
   })
 
   def build
-    language_builder.build
-    build_namespaces
+    init_build
+    start_building_loop
   end
 
-  def build_namespaces
-    while ast = data_source.read
-      ANamespaceBuilder.new(ast: ast).build
+  def init_build
+    @language_builder.build
+  end
+
+  def start_building_loop
+    while ast = @data_source.read
+      @top_level_builder.build(@parser.parse(ast))
     end
   end
 
