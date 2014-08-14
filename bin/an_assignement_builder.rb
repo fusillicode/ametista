279131d60872d404e27698d3c2897c59bc6df9b2
fusillicode <fusillicode@gmail.com>
2014-08-14 16:46:31 +0200

    initial an_assignement_builder

--- bin/an_assignement_builder.rb
+++ bin/an_assignement_builder.rb
@@ -6,8 +6,21 @@ class AnAssignementBuilder
   extend Initializer
   initialize_with ({
     ast: nil,
+    parent_unique_name: '\\',
     querier: nil,
   })
+
+  def build args
+    @ast ||= args[:ast]
+    @parent_unique_name ||= args[:parent_unique_name]
+  end
+
+  def assignement
+    # AnAssignement.find_or_create_by(
+    #   unique_name: '\\',
+    #   name: '\\'
+    # )
+  end
 
 end
 
