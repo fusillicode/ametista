56f6c565b0ec35cc9f4c95e33ef54b8a168956c4
fusillicode <fusillicode@gmail.com>
2014-08-16 10:44:38 +0200

    assignement builder seems little in confront to the namespace one

--- bin/an_assignement_builder.rb
+++ bin/an_assignement_builder.rb
@@ -1,25 +1,31 @@
 require_relative 'utilities'
 require_relative 'model'
 require_relative 'an_assignement_ast_querier'
+require_relative 'a_variable_builder'
 
 class AnAssignementBuilder
 
   extend Initializer
   initialize_with ({
     querier: AnAssignementAstQuerier.new,
-    a_variable_builder: AnAssignementBuilder.new
+    a_variable_builder: AVariableBuilder.new
   })
 
   def build brick
-    querier.brick = brick
+    @querier.brick = brick
     assignement
   end
 
   def assignement
-    AnAssignement.find_or_create_by(
-      unique_name: '\\',
-      name: '\\'
+    assignement = AnAssignement.create(
+      variable: variable,
+      rhs: querier.rhs
     )
+  end
+
+  def variable
+    @querier.brick.ast = querier.variable
+    a_variable_builder.build(querier.brick)
   end
 
 end
