3d6a9317da249ca0e5b4da5cee4a59a21f4a81f7
fusillicode <fusillicode@gmail.com>
2014-08-16 09:47:04 +0200

    move stuff from namespace ast querier to assignement ast querier

--- bin/a_namespace_ast_querier.rb
+++ bin/a_namespace_ast_querier.rb
@@ -28,41 +28,6 @@ class ANamespaceAstQuerier < Querier
     ast.xpath('./subNode:stmts/scalar:array/node:Expr_Assign/subNode:var')
   end
 
-  def global_variable_value
-    ast.xpath('./subNode:expr')
-  end
-
-  def variable_name(ast)
-
-    node = ast.xpath('./*[1]')[0]
-
-    case node.name
-    when 'Expr_Variable'
-      name = node.xpath('./subNode:name/scalar:string').text
-      # non tratto le variabili di variabli (e.g. $$v)
-      return false if name.nil? or name.empty?
-      # le variabili plain assegnate sono comunque in GLOBALS
-      return "GLOBALS[#{name}]" if name != 'GLOBALS'
-      name
-    when 'Expr_PropertyFetch'
-      # non tratto proprietà con nomi dinamici
-      return false if !var = variable_name(node.xpath('./subNode:var'))
-      return false if !name = variable_name(node.xpath('./subNode:name'))
-      var << '->' << name
-    when 'Expr_ArrayDimFetch'
-      # non tratto indici di array dinamici
-      return var if !var = variable_name(node.xpath('./subNode:var'))
-      dim = node.xpath('./subNode:dim/*[name() = "node:Scalar_String" or name() = "node:Scalar_LNumber"]/subNode:value/*').text
-      return false if dim.nil? or dim.empty?
-      var << '[' << dim << ']'
-    when 'string'
-      node.text
-    else
-      false
-    end
-
-  end
-
   def functions
     ast.xpath('./subNode:stmts/scalar:array/node:Stmt_Function')
   end
--- bin/an_assignement_ast_querier.rb
+++ bin/an_assignement_ast_querier.rb
@@ -5,7 +5,38 @@ require_relative 'querier'
 
 class AnAssignementAstQuerier < Querier
 
-  def get_variable(ast)
+  def global_variable_value
+    ast.xpath('./subNode:expr')
+  end
+
+  def variable_name(ast)
+
+    node = ast.xpath('./*[1]')[0]
+
+    case node.name
+    when 'Expr_Variable'
+      name = node.xpath('./subNode:name/scalar:string').text
+      # non tratto le variabili di variabli (e.g. $$v)
+      return false if name.nil? or name.empty?
+      # le variabili plain assegnate sono comunque in GLOBALS
+      return "GLOBALS[#{name}]" if name != 'GLOBALS'
+      name
+    when 'Expr_PropertyFetch'
+      # non tratto proprietà con nomi dinamici
+      return false if !var = variable_name(node.xpath('./subNode:var'))
+      return false if !name = variable_name(node.xpath('./subNode:name'))
+      var << '->' << name
+    when 'Expr_ArrayDimFetch'
+      # non tratto indici di array dinamici
+      return var if !var = variable_name(node.xpath('./subNode:var'))
+      dim = node.xpath('./subNode:dim/*[name() = "node:Scalar_String" or name() = "node:Scalar_LNumber"]/subNode:value/*').text
+      return false if dim.nil? or dim.empty?
+      var << '[' << dim << ']'
+    when 'string'
+      node.text
+    else
+      false
+    end
 
   end
 
