d6d6b7231ebc7368830e5a43858ea60a67f58e68
fusillicode <fusillicode@gmail.com>
2014-08-18 14:29:28 +0200

    add output_class to XMLParser

--- lib/inferer/xml_parser.rb
+++ lib/inferer/xml_parser.rb
@@ -1,6 +1,12 @@
+require_relative 'utilities'
 require 'nokogiri'
 
 class XMLParser
+
+  extend Initializer
+  initialize_with ({
+    output_class: Nokogiri::XML::Document
+  })
 
   def parse ast
     Nokogiri::XML(ast)
