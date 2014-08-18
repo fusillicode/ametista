6e0648b606025c88a09eb5c6f12fcb2c24ff953a
fusillicode <fusillicode@gmail.com>
2014-08-18 17:47:04 +0200

    simplify XMLParser as a result of simplyfing ASTDecorator

--- lib/inferer/xml_parser.rb
+++ lib/inferer/xml_parser.rb
@@ -1,12 +1,6 @@
-require_relative 'utilities'
 require 'nokogiri'
 
 class XMLParser
-
-  extend Initializer
-  initialize_with ({
-    output_class: Nokogiri::XML::Document
-  })
 
   def parse ast
     Nokogiri::XML(ast)
