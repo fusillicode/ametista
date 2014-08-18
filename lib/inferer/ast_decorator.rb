6b2b1e28e92c10b9bd934e5c45368770e09142a2
fusillicode <fusillicode@gmail.com>
2014-08-18 14:27:54 +0200

    goodbye Brick, welcome ASTDecorator

---
+++ lib/inferer/ast_decorator.rb
@@ -0,0 +1,32 @@
+require_relative 'utilities'
+
+class ASTDecorator
+
+  extend Initializer
+  initialize_with ({
+    ast: nil,
+    parser: XMLParser.new,
+    global_namespace_name: '\\',
+    global_namespace_unique_name: '\\'
+  })
+
+  def decore ast
+    @ast = is_decored? ast ? ast : parser.parse(ast)
+    self
+  end
+
+  def is_decored? ast
+    ast.instance_of? parser.output_class
+  end
+
+  def method_missing method_name, *args, &block
+    if self.respond_to? method_name
+      public_send method_name, *args, &block
+    elsif ast.respond_to? method_name
+      ast.public_send method_name, *args, &block
+    else
+      super
+    end
+  end
+
+end
