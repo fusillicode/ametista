be7d98344bd574239f0c84ed4047442a7bd257d3
fusillicode <fusillicode@gmail.com>
2014-08-18 14:28:49 +0200

    every Builder will decore the ast passed to it if the ast isn't already decored

--- lib/inferer/builders/a_namespace_builder.rb
+++ lib/inferer/builders/a_namespace_builder.rb
@@ -1,6 +1,5 @@
 require_relative '../utilities'
 require_relative '../schema'
-require_relative '../brick'
 require_relative '../queriers/a_namespace_ast_querier'
 require_relative 'a_branch_builder'
 require_relative 'an_assignement_builder'
@@ -18,17 +17,16 @@ class ANamespaceBuilder
     a_class_builder: AClassBuilder.new
   })
 
-  def build brick
-    @querier.brick = brick
+  def build ast
+    @querier.ast_decorator.decore(ast)
     global_namespace
     namespaces
   end
 
   def global_namespace
-    querier.parent_as_root
     global_namespace = ANamespace.find_or_create_by(
-      unique_name: querier.root_unique_name,
-      name: querier.root_unique_name
+      unique_name: querier.global_namespace_unique_name,
+      name: querier.global_namespace_name
     )
     # global_namespace.assignements.concat(assignements)
     # global_namespace.branches.concat(branches)
@@ -38,18 +36,16 @@ class ANamespaceBuilder
   end
 
   def namespaces
-    querier.parent_as_root
     querier.namespaces.map do |namespace_ast|
-      @querier.brick.ast = namespace_ast
-      @querier.parent_unique_name = querier.namespace_unique_name
+      @querier.ast = namespace_ast
       namespace
     end
   end
 
   def namespace
     namespace = ANamespace.find_or_create_by(
-      unique_name: querier.parent_unique_name,
-      name: querier.parent_name
+      unique_name: querier.namespace_unique_name,
+      name: querier.namespace_name
     )
     namespace.assignements.concat(assignements)
     # namespace.branches.concat(branches)
@@ -60,29 +56,25 @@ class ANamespaceBuilder
 
   def assignements()
     querier.assignements.map do |assignement_ast|
-      @querier.brick.ast = assignement_ast
-      an_assignement_builder.build(querier.brick)
+      an_assignement_builder.build(assignement_ast)
     end
   end
 
   def branches
     querier.branches.map do |branch_ast|
-      @querier.brick.ast = branch_ast
-      a_branch_builder.build(querier.brick)
+      a_branch_builder.build(branch_ast)
     end
   end
 
   def functions
     querier.functions.map do |function_ast|
-      @querier.brick.ast = function_ast
-      a_function_builder.build(querier.brick)
+      a_function_builder.build(function_ast)
     end
   end
 
   def classes
     querier.classes.map do |class_ast|
-      @querier.brick.ast = class_ast
-      a_class_builder.build(querier.brick)
+      a_class_builder.build(class_ast)
     end
   end
 
