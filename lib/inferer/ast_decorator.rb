2b4be3d6b5d002b78ae58b1b186bbd21c715ed36
fusillicode <fusillicode@gmail.com>
2014-08-18 17:45:55 +0200

    ASTDecorator should not parse!

--- lib/inferer/ast_decorator.rb
+++ lib/inferer/ast_decorator.rb
@@ -6,19 +6,9 @@ class ASTDecorator
   extend Initializer
   initialize_with ({
     ast: nil,
-    parser: XMLParser.new,
     global_namespace_name: '\\',
     global_namespace_unique_name: '\\'
   })
-
-  def decore ast
-    @ast = is_decored?(ast) ? ast : parser.parse(ast)
-    self
-  end
-
-  def is_decored? ast
-    ast.instance_of? parser.output_class
-  end
 
   # TODO implementare anche respond_to?
   def method_missing method_name, *args, &block
