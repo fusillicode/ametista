37a5354894d5db32b3f7b5e955cb930a3923d03d
fusillicode <fusillicode@gmail.com>
2014-08-19 16:29:03 +0200

    parent_name an unique for functions

--- lib/inferer/queriers/function_ast_querier.rb
+++ lib/inferer/queriers/function_ast_querier.rb
@@ -1,6 +1,18 @@
 require_relative 'querier'
 
-class FunctionAstQuerier
+class FunctionAstQuerier < Querier
+
+  def functions
+    ast.xpath('.//subNode:stmts/scalar:array/node:Stmt_Function')
+  end
+
+  def parent_name ast
+    ast.xpath('./ancestor::node:Stmt_Namespace[1]/subNode:name/node:Name/subNode:parts/scalar:array/scalar:string[last()]').text
+  end
+
+  def parent_unique_name ast
+    global_namespace_unique_name + ast.xpath('./ancestor::node:Stmt_Namespace[1]/subNode:name/node:Name/subNode:parts/scalar:array/scalar:string')[0..-1].to_a.join('\\')
+  end
 
   def name(ast)
     ast.xpath('./subNode:name/scalar:string').text
