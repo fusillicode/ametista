df1d705225d86a0887488453c159b68d07294fc6
fusillicode <fusillicode@gmail.com>
2014-08-20 15:13:35 +0200

    custom type ast querier

--- lib/inferer/queriers/custom_type_ast_querier.rb
+++ lib/inferer/queriers/custom_type_ast_querier.rb
@@ -3,8 +3,12 @@ require_relative 'querier'
 class CustomTypeAstQuerier < Querier
 
   def custom_types
-    ast.xpath(".//node:Param[subNode:type/node:Name_FullyQualified/subNode:parts/scalar:array/scalar:string[last()][not(contains('#{types.join("','")}'))]]") +
+    ast.xpath(".//node:Param[subNode:type/node:Name_FullyQualified/subNode:parts/scalar:array/scalar:string[last()][not(#{basic_types})]]") +
     ast.xpath(".//node:Stmt_Class")
+  end
+
+  def basic_types
+    ".='" + language.types.join("' or .='") + "'"
   end
 
   def name ast
@@ -13,8 +17,9 @@ class CustomTypeAstQuerier < Querier
   end
 
   def unique_name ast
-    ast.xpath('./subNode:type/node:Name_FullyQualified/subNode:parts/scalar:array/scalar:string')[0..-1].to_a.join('\\') ||
+    unique_name = ast.xpath('./subNode:type/node:Name_FullyQualified/subNode:parts/scalar:array/scalar:string')[0..-1].to_a.join('\\') ||
     ast.xpath('./subNode:namespacedName/node:Name/subNode:parts/scalar:array/scalar:string')[0..-1].to_a.join('\\')
+    global_namespace_unique_name + unique_name
   end
 
 end
