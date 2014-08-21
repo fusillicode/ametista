6f69576d48525f3f1a30a53a8a878a789470e770
fusillicode <fusillicode@gmail.com>
2014-08-21 09:49:56 +0200

    builders_loop in model builder

--- lib/inferer/builders/model_builder.rb
+++ lib/inferer/builders/model_builder.rb
@@ -3,13 +3,17 @@ require_relative '../utilities'
 require_relative '../redis_data_source'
 require_relative '../xml_parser'
 require_relative 'language_builder'
-require_relative 'namespace_builder'
-require_relative 'function_builder'
-require_relative 'custom_type_builder'
-require_relative 'klass_builder'
-require_relative 'kmethod_builder'
-require_relative 'branch_builder'
-require_relative 'assignement_builder'
+require_relative 'namespaces_builder'
+require_relative 'functions_builder'
+require_relative 'custom_types_builder'
+require_relative 'parameters_builder'
+require_relative 'global_variables_builder'
+require_relative 'local_variables_builder'
+require_relative 'properties_builder'
+require_relative 'klasses_builder'
+require_relative 'kmethods_builder'
+require_relative 'branches_builder'
+require_relative 'assignements_builder'
 
 class ModelBuilder
 
@@ -18,40 +22,41 @@ class ModelBuilder
     parser: XMLParser.new,
     data_source: RedisDataSource.new,
     language_builder: LanguageBuilder.new,
-    namespaces_builder: NamespaceBuilder.new,
-    functions_builder: FunctionBuilder.new,
-    custom_types_builder: CustomTypeBuilder.new,
-    classes_builder: KlassBuilder.new,
-    methods_builder: KMethodBuilder.new,
-    branches_builder: BranchBuilder.new,
-    assignements_builder: AssignementBuilder.new
+    builders: {
+      namespaces_builder: NamespacesBuilder.new,
+      functions_builder: FunctionsBuilder.new,
+      custom_types_builder: CustomTypesBuilder.new
+      # parameters_builder: ParametersBuilder.new,
+      # global_variables_builder: GlobalVariablesBuilder.new,
+      # local_variables_builder: LocalVariablesBuilder.new,
+      # properties_builder: PropertiesBuilder.new,
+      # klasses_builder: KlassesBuilder.new,
+      # kmethods_builder: KMethodsBuilder.new,
+      # branches_builder: BranchesBuilder.new,
+      # assignements_builder: AssignementsBuilder.new
+    }
   })
 
   def build
     init_build
-    start_building_loop
+    building_loop
   end
 
   def init_build
     language_builder.build
   end
 
-  def start_building_loop
+  def builders_loop ast
+    builders.each do |key, builder|
+      p builder.build(ast)
+    end
+  end
+
+  def building_loop
     while ast = data_source.read
       # TODO togliere il break e la condizione una volta sistemato data_source.read
       break if ast == "THAT'S ALL FOLKS!"
-      ast = parser.parse(ast)
-      namespaces_builder.build(ast)
-      functions_builder.build(ast)
-      custom_types_builder.build(ast)
-      # parameters_builder.build(ast)
-      # global_variables_builder.build(ast)
-      # local_variables_builder.build(ast)
-      # properties_variables_builder.build(ast)
-      # klasses_builder.build(ast)
-      # kmethods_builder.build(ast)
-      # branches_builder.build(ast)
-      # assignements_builder.build(ast)
+      builders_loop(parser.parse(ast))
     end
   end
 
