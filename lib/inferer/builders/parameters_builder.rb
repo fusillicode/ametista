7bf216b4a553f7757a528f62193f053ab09241ff
fusillicode <fusillicode@gmail.com>
2014-08-22 14:55:43 +0200

    test parameters builder

--- lib/inferer/builders/parameters_builder.rb
+++ lib/inferer/builders/parameters_builder.rb
@@ -22,16 +22,12 @@ class ParametersBuilder
         name: querier.name(parameter_ast),
         procedure: parent(parameter_ast)
       )
-      # parameter.types << type(ast)
+      parameter.types << type(parameter_ast)
       parameter
     end
   end
 
   def parent ast
-    # p querier.parent_type(ast)
-    # p querier.parent_name(ast)
-    # p querier.parent_unique_name(ast)
-    p querier.parent_type(ast)
     querier.parent_type(ast).find_or_create_by(
       unique_name: querier.parent_unique_name(ast),
       name: querier.parent_name(ast)
