493153c98a3ab721ea802f6ad486cbdac2913d14
fusillicode <fusillicode@gmail.com>
2014-08-22 10:55:22 +0200

    entity mapper initialization for basic types

--- lib/inferer/entity_mapper.rb
+++ lib/inferer/entity_mapper.rb
@@ -1,13 +1,19 @@
 require_relative 'utilities'
 require_relative 'php_language'
+require_relative 'schema'
 
 class EntityMapper
   extend Initializer
   initialize_with ({
     language: PHPLanguage.new,
-    map: {
-      'node:Stmt_Function' => Function,
-      'node:Stmt_Class'    => Klass
-    }
+    map: Hash.new(CustomType)
   })
+
+  def initialize
+    super
+    @language.types.each do |basic_type|
+      @map[basic_type.to_sym] = BasicType
+    end
+  end
+
 end
