9288d039b6ee8e14c14571b87cc2b0daf3effe84
fusillicode <fusillicode@gmail.com>
2014-09-06 11:28:58 +0200

    refactoring custom_types_ast_querier

--- lib/inferer/queriers/custom_types_ast_querier.rb
+++ lib/inferer/queriers/custom_types_ast_querier.rb
@@ -3,7 +3,26 @@ require_relative 'querier'
 class CustomTypesAstQuerier < Querier
 
   def custom_types ast
-    ast.xpath(".//node:Param[subNode:type/node:Name_FullyQualified/subNode:parts/scalar:array/scalar:string[last()][not(#{basic_types_list('or')})]]") +
+    parameters_custom_types(ast) + klasses(ast)
+  end
+
+  def name ast
+    parameter_type_name = parameter_custom_type_name(ast)
+    return parameter_type_name if parameter_type_name != ''
+    klass_name(ast)
+  end
+
+  def unique_name ast
+    parameter_type_unique_name = parameter_custom_type_unique_name(ast)
+    return parameter_type_unique_name if parameter_type_unique_name != ''
+    klass_unique_name(ast)
+  end
+
+  def parameters_custom_types ast
+    ast.xpath(".//node:Param[subNode:type/node:Name_FullyQualified/subNode:parts/scalar:array/scalar:string[last()][not(#{basic_types_list('or')})]]")
+  end
+
+  def klasses ast
     ast.xpath(".//node:Stmt_Class")
   end
 
@@ -11,15 +30,19 @@ class CustomTypesAstQuerier < Querier
     ".='" + language.types.join("' #{operator} .='") + "'"
   end
 
-  def name ast
-    parameter_type_name = ast.xpath('./subNode:type/node:Name_FullyQualified/subNode:parts/scalar:array/scalar:string[last()]').text
-    return parameter_type_name if parameter_type_name != ''
+  def parameter_custom_type_name ast
+    ast.xpath('./subNode:type/node:Name_FullyQualified/subNode:parts/scalar:array/scalar:string[last()]').text
+  end
+
+  def klass_name ast
     ast.xpath('./subNode:name/scalar:string').text
   end
 
-  def unique_name ast
-    parameter_type_unique_name = ast.xpath('./subNode:type/node:Name_FullyQualified/subNode:parts/scalar:array/scalar:string')[0..-1].to_a.join('\\')
-    return parameter_type_unique_name if parameter_type_unique_name != ''
+  def parameter_custom_type_unique_name ast
+    ast.xpath('./subNode:type/node:Name_FullyQualified/subNode:parts/scalar:array/scalar:string')[0..-1].to_a.join('\\')
+  end
+
+  def klass_unique_name ast
     ast.xpath('./subNode:namespacedName/node:Name/subNode:parts/scalar:array/scalar:string')[0..-1].to_a.join('\\')
   end
 
