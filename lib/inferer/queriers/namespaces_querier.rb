d74f2f3f6443a490dbd2a60ba6eb33f9ecf88060
fusillicode <fusillicode@gmail.com>
2014-09-12 10:23:30 +0200

    namespace separator and clean up

--- lib/inferer/queriers/namespaces_querier.rb
+++ lib/inferer/queriers/namespaces_querier.rb
@@ -11,7 +11,7 @@ class NamespacesQuerier < Querier
   end
 
   def unique_name ast
-    "#{global_namespace_unique_name}#{ast.xpath('./subNode:name/node:Name/subNode:parts/scalar:array/scalar:string')[0..-1].to_a.join('\\')}"
+    "#{global_namespace_unique_name}#{language.namespace_separator}#{ast.xpath('./subNode:name/node:Name/subNode:parts/scalar:array/scalar:string')[0..-1].to_a.join('\\')}"
   end
 
   def statements ast
@@ -21,22 +21,5 @@ class NamespacesQuerier < Querier
   def global_namespace_statements ast
     ast.xpath('/AST/scalar:array/*[name() != "node:Stmt_Function" and name() != "node:Stmt_Class"]')
   end
-
-  # def inline_namespaces
-  #   ast.xpath('./subNode:name/node:Name/subNode:parts/scalar:array/scalar:string')
-  # end
-
-  # # le variabili assegnate
-  # def assignements
-  #   ast.xpath('./subNode:stmts/scalar:array/node:Expr_Assign')
-  # end
-
-  # def functions
-  #   ast.xpath('./subNode:stmts/scalar:array/node:Stmt_Function')
-  # end
-
-  # def classes
-  #   ast.xpath('./subNode:stmts/scalar:array/node:Stmt_Class')
-  # end
 
 end
