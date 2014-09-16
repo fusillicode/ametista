7a0fd57d84fa89d8845caa0d974ff901b24a07dc
fusillicode <fusillicode@gmail.com>
2014-09-16 15:42:09 +0200

    require on top

--- bin/build.rb
+++ bin/build.rb
@@ -1,9 +1,9 @@
 if __FILE__ == $0
   require 'mongoid'
   require_relative '../lib/inferer/mongo_daemon'
+  require_relative '../lib/inferer/builders/model_builder'
   mongo_daemon = MongoDaemon.new.start
   Mongoid.load!('./mongoid.yml', :development)
   Mongoid::Config.purge!
-  require_relative '../lib/inferer/builders/model_builder'
   model_builder = ModelBuilder.new.build
 end
