b9677e31a998c8c0a084d682a8eae4a466f6f1d4
fusillicode <fusillicode@gmail.com>
2014-09-16 15:41:15 +0200

    fix build issue

--- bin/build.rb
+++ bin/build.rb
@@ -1,10 +1,9 @@
-require 'mongoid'
-Mongoid.load!('./mongoid.yml', :development)
-Mongoid::Config.purge!
-
 if __FILE__ == $0
+  require 'mongoid'
   require_relative '../lib/inferer/mongo_daemon'
   mongo_daemon = MongoDaemon.new.start
+  Mongoid.load!('./mongoid.yml', :development)
+  Mongoid::Config.purge!
   require_relative '../lib/inferer/builders/model_builder'
   model_builder = ModelBuilder.new.build
 end
