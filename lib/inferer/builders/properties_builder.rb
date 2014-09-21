253634844df912e5c22388c90835ea073f2c6aa2
fusillicode <fusillicode@gmail.com>
2014-09-21 11:39:39 +0200

    initial properties builder and querier

--- lib/inferer/builders/properties_builder.rb
+++ lib/inferer/builders/properties_builder.rb
@@ -3,7 +3,7 @@ require_relative '../utilities'
 require_relative '../schema'
 require_relative '../queriers/properties_querier'
 
-class PropertiesBuilder
+class PropertiesBuilder < Builder
 
   extend Initializer
   initialize_with ({
@@ -12,6 +12,24 @@ class PropertiesBuilder
 
   def build ast
     @ast = ast
+    static_properties
+  end
+
+  def properties
+    querier.properties(ast).map_unique do |property_ast|
+      Property.find_or_create_by(
+        unique_name: querier.unique_name(property_ast),
+        name: querier.name(property_ast),
+        statements: querier.statements(property_ast)
+      )
+    end
+  end
+
+  def klass property_ast
+    Klass.find_or_create_by(
+      unique_name: querier.klass_unique_name(property_ast),
+      name: querier.klass_name(property_ast)
+    )
   end
 
 end
--- lib/inferer/queriers/properties_querier.rb
+++ lib/inferer/queriers/properties_querier.rb
@@ -1,7 +1,42 @@
 require_relative 'querier'
 
-class PropertiesQuerier
+class PropertiesQuerier < Querier
 
+  def objects_properties ast_root
+    ast_root.xpath(".//node:Expr_PropertyFetch/subNode:var[last()]/node:Expr_Variable/subNode:name/scalar:string[#{object_property}]")
+  end
+
+  def self_class_properties ast_root
+    ast_root.xpath(".//node:Expr_StaticPropertyFetch/subNode:var[last()]/node:Expr_Variable/subNode:name/scalar:string[#{self_class_property}]")
+  end
+
+  def parent_class_property ast_root
+    ast_root.xpath(".//node:Expr_StaticPropertyFetch/subNode:var[last()]/node:Expr_Variable/subNode:name/scalar:string[#{parent_class_property}]")
+  end
+
+  def static_class_property ast_root
+    ast_root.xpath(".//node:Expr_StaticPropertyFetch/subNode:var[last()]/node:Expr_Variable/subNode:name/scalar:string[#{static_class_property}]")
+  end
+
+  def class_property ast_root
+    ast_root.xpath(".//node:Expr_StaticPropertyFetch/subNode:var[last()]/node:Expr_Variable/subNode:name/scalar:string[#{class_property}]")
+  end
+
+  def name ast
+    ast.xpath('./subNode:name/scalar:string').text
+  end
+
+  def unique_name ast
+    "#{global_namespace_unique_name}#{namespace_separator}#{ast.xpath('./subNode:namespacedName/node:Name/subNode:parts/scalar:array/scalar:string')[0..-1].to_a.join('\\')}"
+  end
+
+  def klass_name ast
+    ast.xpath('./ancestor::node:Stmt_Class[1]/subNode:name/scalar:string').text
+  end
+
+  def klass_unique_name ast
+    "#{global_namespace_unique_name}#{namespace_separator}#{ast.xpath('./ancestor::node:Stmt_Class[1]/subNode:namespacedName/node:Name/subNode:parts/scalar:array/scalar:string')[0..-1].to_a.join('\\')}"
+  end
 
 end
 
