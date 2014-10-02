77e0a64f0cf950b078091a1415db8c18d8054bde
fusillicode <fusillicode@gmail.com>
2014-10-02 13:15:21 +0200

    build namespace local variables

--- lib/ametista/builders/local_variables_builder.rb
+++ lib/ametista/builders/local_variables_builder.rb
@@ -16,13 +16,26 @@ class LocalVariablesBuilder < Builder
   end
 
   def local_variables
-    functions_local_variables << klasses_methods_local_variables
+    namespaces_local_variables # << functions_local_variables << klasses_methods_local_variables
   end
 
   # TODO vedere di ristrutturare il building delle variabili locali (come anche dei parametri) tirando fuori prima i loro parent evitando così di fare troppe query xpath relative ai parent (i.e. funzioni e metodi di classe).
 
+  def namespaces_local_variables
+    querier.namespaces_local_variables(ast).map_unique do |namespace_local_variable_ast|
+      p querier.namespace_local_variable_name(namespace_local_variable_ast)
+      LocalVariable.find_or_create_by(
+        name: querier.namespace_local_variable_name(namespace_local_variable_ast),
+        local_scope: namespace(namespace_local_variable_ast)
+      )
+    end
+  end
+
   def functions_local_variables
     querier.functions_local_variables(ast).map_unique do |function_local_variable_ast|
+      # LocalVariable.where(
+      #   unique_name: querier.function_local_variable_unique_name(function_local_variable_ast)
+      # ).exists?
       LocalVariable.find_or_create_by(
         name: querier.function_local_variable_name(function_local_variable_ast),
         local_scope: function(function_local_variable_ast)
@@ -50,6 +63,13 @@ class LocalVariablesBuilder < Builder
     KlassMethod.find_or_create_by(
       unique_name: querier.klass_method_unique_name(klass_method_local_variable_ast),
       name: querier.klass_method_name(klass_method_local_variable_ast)
+    )
+  end
+
+  def namespace namespace_local_variable_ast
+    Namespace.find_or_create_by(
+      unique_name: querier.namespace_unique_name(namespace_local_variable_ast),
+      name: querier.namespace_name(namespace_local_variable_ast)
     )
   end
 
