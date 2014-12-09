0ee2e693dbc404bc839b488b285ab9c182d07ec7
fusillicode <fusillicode@gmail.com>
2014-12-09 14:30:14 +0100

    local_variables builder rely on other builders

--- lib/ametista/builders/local_variables_builder.rb
+++ lib/ametista/builders/local_variables_builder.rb
@@ -2,12 +2,18 @@ require_relative 'builder'
 require_relative '../utilities'
 require_relative '../schema'
 require_relative '../queriers/local_variables_querier'
+require_relative 'namespaces_builder'
+require_relative 'functions_builder'
+require_relative 'klasses_methods_builder'
 
 class LocalVariablesBuilder < Builder
 
   extend Initializer
   initialize_with ({
-    querier: LocalVariablesQuerier.new
+    querier: LocalVariablesQuerier.new,
+    namespaces_builder: NamespacesBuilder.new,
+    functions_builder: FunctionsBuilder.new,
+    klasses_methods_builder: KlassesMethodsBuilder.new
   })
 
   def build ast
@@ -36,7 +42,10 @@ class LocalVariablesBuilder < Builder
     querier.functions_local_variables(ast).map_unique('_id') do |function_local_variable_ast|
       local_variable = LocalVariable.find_or_create_by(
         name: querier.function_local_variable_name(function_local_variable_ast),
-        local_scope: function(function_local_variable_ast)
+        # local_scope: function(function_local_variable_ast),
+        local_scope: functions_builder.function(
+          querier.function(function_local_variable_ast)
+        )
       )
       # TODO decoupling fra variabili locali e i loro assegnamenti creando magari un AssignementBuilder
       Assignement.create(
@@ -52,29 +61,11 @@ class LocalVariablesBuilder < Builder
     querier.klasses_methods_local_variables(ast).map_unique('_id') do |klass_method_local_variable_ast|
       LocalVariable.find_or_create_by(
         name: querier.klass_method_local_variable_name(klass_method_local_variable_ast),
-        local_scope: klass_method(klass_method_local_variable_ast)
+        local_scope: klasses_methods_builder.klass_method(
+          querier.klass_method(klass_method_local_variable_ast)
+        )
       )
     end
-  end
-
-  def function function_local_variable_ast
-    Function.find_or_create_by(
-      unique_name: querier.function_unique_name(function_local_variable_ast),
-      name: querier.function_name(function_local_variable_ast)
-    )
-  end
-
-  def klass_method klass_method_local_variable_ast
-    KlassMethod.find_or_create_by(
-      unique_name: querier.klass_method_unique_name(klass_method_local_variable_ast),
-      name: querier.klass_method_name(klass_method_local_variable_ast)
-    )
-  end
-
-  def namespace namespace_local_variable_ast
-    Namespace.find_or_create_by(
-      unique_name: querier.namespace_unique_name(namespace_local_variable_ast),
-    )
   end
 
 end
--- lib/ametista/queriers/local_variables_querier.rb
+++ lib/ametista/queriers/local_variables_querier.rb
@@ -40,48 +40,12 @@ class LocalVariablesQuerier < AssignementQuerier
     ast.xpath('./subNode:name/scalar:string').text
   end
 
-  def function_local_variable_unique_name ast
-    "#{function_unique_name(ast)}#{namespace_separator}#{function_local_variable_name(ast)}"
+  def function ast
+    ast.xpath("./ancestor::node:Stmt_Function[1]")
   end
 
-  def function_unique_name ast
-    "#{namespace_unique_name(ast)}#{namespace_separator}#{function_name(ast)}"
-  end
-
-  def function_name ast
-    ast.xpath("./ancestor::node:Stmt_Function[1]/subNode:name/scalar:string").text
-  end
-
-  def namespace_local_variable_name ast
-    ast.xpath('./subNode:name/scalar:string').text
-  end
-
-  def namespace_local_variable_unique_name ast
-    "#{namespace_unique_name(ast)}#{namespace_separator}#{namespace_local_variable_name(ast)}"
-  end
-
-  def klass_method_local_variable_name ast
-    ast.xpath('./subNode:name/scalar:string').text
-  end
-
-  def klass_method_local_variable_unique_name ast
-    "#{klass_method_unique_name(ast)}#{namespace_separator}#{klass_method_local_variable_name(ast)}"
-  end
-
-  def klass_method_unique_name ast
-    "#{klass_unique_name(ast)}#{namespace_separator}#{klass_method_name(ast)}"
-  end
-
-  def klass_unique_name ast
-    "#{global_namespace_unique_name}#{namespace_separator}#{klass_namespaced_name(ast)}"
-  end
-
-  def klass_namespaced_name ast
-    ast.xpath('./ancestor::node:Stmt_Class[1]/subNode:namespacedName/node:Name/subNode:parts/scalar:array/scalar:string')[0..-1].to_a.join(namespace_separator)
-  end
-
-  def klass_method_name ast
-    ast.xpath("./ancestor::node:Stmt_ClassMethod[1]/subNode:name/scalar:string").text
+  def klass_method ast
+    ast.xpath("./ancestor::node:Stmt_ClassMethod[1]")
   end
 
 end
