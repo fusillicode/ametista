2abe429220be623b398630b58347a82349cf7e78
fusillicode <fusillicode@gmail.com>
2014-12-11 11:55:08 +0100

    add jsonify capacity to builders

--- lib/ametista/builders/builder.rb
+++ lib/ametista/builders/builder.rb
@@ -1,9 +1,14 @@
+require 'forwardable'
 require_relative '../queriers/querier'
+require_relative '../jsonifier'
 
 class Builder
   extend Initializer
+  extend Forwardable
   initialize_with ({
     querier: Querier.new,
+    jsonifier: JSONifier.new,
     ast: nil
   })
+  def_delegator :@jsonifier, :jsonify, :jsonify
 end
