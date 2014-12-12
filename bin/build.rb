1c58618bf3a601d93b6bae42302817feebd0f663
fusillicode <fusillicode@gmail.com>
2014-12-12 22:21:14 +0100

    new build

--- bin/build.rb
+++ bin/build.rb
@@ -1,9 +1,9 @@
 if __FILE__ == $0
-  require 'mongoid'
-  require_relative '../lib/ametista/mongo_daemon'
-  require_relative '../lib/ametista/builders/model_builder'
-  mongo_daemon = MongoDaemon.new.start
-  Mongoid.load!('./mongoid.yml', :development)
-  Mongoid::Config.purge!
-  model_builder = ModelBuilder.new.build
+  require 'active_record'
+  require 'yaml'
+  config_file = File.join Dir.pwd, 'db', 'config.yml'
+  ENV['DB'] = 'development'
+  config = YAML::load(File.open config_file)
+  ActiveRecord::Base.establish_connection config[ENV['DB']]
+  # model_builder = ModelBuilder.new.build
 end
