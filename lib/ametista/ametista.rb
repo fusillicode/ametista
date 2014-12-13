4d2bc3a09e1465a8216df635e3321180db9e53cd
fusillicode <fusillicode@gmail.com>
2014-12-13 18:44:58 +0100

    simplify thor shine

--- lib/ametista/ametista.rb
+++ lib/ametista/ametista.rb
@@ -3,17 +3,19 @@ require "ametista/version"
 module Ametista
   class Shine < Thor
     desc "hello NAME", "say hello to NAME"
-    options :first_option => :required, :yell => :boolean
-    def parse name
+    options :directory => :required
+    def parse
 
     end
 
     desc "goodbye", "say goodbye to the world"
+    options :config_files :array
     def build
-
+      # model_builder = ModelBuilder.new.build
     end
 
     desc "goodbye", "say goodbye to the world"
+    options :first_option => :required, :yell => :boolean
     def analyze
 
     end
