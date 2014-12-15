e4105fd94d674d11028ab0104df3cd38438d969e
fusillicode <fusillicode@gmail.com>
2014-12-15 11:25:09 +0100

    BROKEN OLD MONGODB SCHEMA

--- lib/ametista/schema.rb
+++ lib/ametista/schema.rb
@@ -1,200 +1,165 @@
-require "mongoid"
-include EnforceAvailableLocales
-
-module HasAUniqueName
+module HasUniqueName
   def self.included base
-    base.include Mongoid::Document
     base.field :unique_name, type: String
-    base.index({ unique_name: 1 }, { unique: true, drop_dups: true })
     base.validates :unique_name, presence: true, length: { allow_blank: false }
   end
 end
 
-module HasAName
+module HasNameAndUniqueName
   def self.included base
-    base.include Mongoid::Document
+    base.include DerivedUniqueName
     base.field :name, type: String
     base.validates :name, presence: true, length: { allow_blank: false }
   end
 end
 
-module ReferencesLanguage
+module DerivedUniqueName
   def self.included base
-    base.include Mongoid::Document
-    base.belongs_to :language, class_name: 'Language'
-    base.after_initialize do
-      reference_language
-    end
-    base.extend ClassMethods
+    base.field :unique_name, type: String
+    base.after_initialize :init
   end
-  module ClassMethods
-    def language
-      @language ||= Language.first()
-    end
-  end
-  def reference_language
-    self.language ||= Language.first()
+  def init
+    self.unique_name = derived_unique_name
   end
 end
 
-module IsSingleton
+module IsGlobalScope
   def self.included base
-    base.include Mongoid::Document
-    base.validate :enforce_singleton
-    def initialize *args
-      enforce_singleton
-      super
-    end
-    def enforce_singleton
-      raise "there can be only one #{self.class}." unless is_singleton?
-    end
-    def is_singleton?
-      self.class.all.count.zero?
-    end
+    base.has_many :global_variables, as: :global_scope
   end
 end
 
-module ContainsGlobalVariables
+module IsLocalScope
   def self.included base
-    base.include Mongoid::Document
-    base.has_many :global_variables, class_name: 'GlobalVariable', as: :global_scope
+    base.has_many :local_variables, as: :local_scope
   end
 end
 
-module ContainsLocalVariables
+module IsProcedure
   def self.included base
-    base.include Mongoid::Document
-    base.has_many :local_variables, class_name: 'LocalVariable', as: :local_scope
+    base.include HasNameAndUniqueName
+    base.include IsLocalScope
+    base.field :statements, type: xml
+    base.has_many :parameters, as: :procedure
   end
 end
 
-module IsAProcedure
+module IsVariable
   def self.included base
-    base.include ReferencesLanguage
-    base.include HasAName
-    base.include ContainsLocalVariables
-    base.field :statements, type: Hash
-    base.has_many :parameters, class_name: 'Parameter', as: :procedure
+    base.include HasNameAndUniqueName
+    base.has_many :types, through: :variable_types
+    base.has_many :assignements, as: :variable
   end
 end
 
-# I need these "abstract" class to handle the n-n relation between them
-# (can't handle the relation with polymorfic)
-class Type
-  include ReferencesLanguage
-  include HasAName
-  has_and_belongs_to_many :variables, class_name: 'Variable', inverse_of: :types
-end
-
-class Variable
-  include ReferencesLanguage
-  include HasAName
-  has_and_belongs_to_many :types, class_name: 'Type', inverse_of: :variables
-  has_many :assignements, class_name: 'Assignement', inverse_of: :variable
-  # has_many :methods_invocations, class_name: 'MethodInvocation', inverse_of: :variable
+module IsType
+  def self.included base
+    base.include HasNameAndUniqueName
+    base.has_many :variables, through: :variable_types
+  end
 end
 
 ################################################################################
 
-class Language
-  include IsSingleton
-  include HasAName
-  include Mongoid::Attributes::Dynamic
-end
-
-class Namespace
-  include ReferencesLanguage
-  include HasAUniqueName
-  field :statements, type: Hash
-  has_many :functions, class_name: 'Function', inverse_of: :namespace
-  has_many :klasses, class_name: 'Klass', inverse_of: :namespace
-  after_initialize do
-    extend contains_variables
-  end
-  def contains_variables
-    is_global_namespace? ? ContainsGlobalVariables : ContainsLocalVariables
+class Namespace < ActiveRecord::Base
+  include HasUniqueName
+  field :statements, type: xml
+  has_many :functions, inverse_of: :namespace
+  has_many :klasses, inverse_of: :namespace
+  after_initialize { extend define_scope_type }
+  def define_scope_type
+    is_global_namespace? ? IsGlobalScope : IsLocalScope
   end
   def is_global_namespace?
     unique_name.eql? language.global_namespace['unique_name']
   end
 end
 
-class Klass < Type
-  belongs_to :namespace, class_name: 'Namespace', inverse_of: :klasses
-  belongs_to :parent_klass, class_name: 'Klass', inverse_of: :child_klasses
-  has_many :child_klasses, class_name: 'Klass', inverse_of: :parent_klass
-  has_many :methods, class_name: 'KlassMethod', inverse_of: :klass
-  has_many :properties, class_name: 'Property', inverse_of: :klass
-  def unique_name
+class Klass < ActiveRecord::Base
+  include IsType
+  belongs_to :namespace, inverse_of: :klasses
+  belongs_to :parent_klass, class_name: 'Klass'
+  has_many :child_klasses, class_name: 'Klass', foreign_key: 'parent_klass_id'
+  has_many :klass_methods, inverse_of: :klass
+  has_many :properties, inverse_of: :klass
+  has_many :variables, through: :variables_types
+  def derived_unique_name
     "#{namespace.unique_name}#{language.namespace_separator}#{name}"
   end
 end
 
-class PrimitiveType < Type
+class PrimitiveType < ActiveRecord::Base
+  include IsType
+end
+
+class VariableType < ActiveRecord::Base
+  belongs_to :variable
+  belongs_to :type
 end
 
 # alias in modo da poter chiamare CustomType e Klass in maniera indifferenziata
 CustomType = Klass
 
-class KlassMethod
-  include IsAProcedure
-  belongs_to :klass, class_name: 'Klass', inverse_of: :methods
-  # has_and_belongs_to_many :methods_invocations, class_name: 'MethodInvocation', inverse_of: :methods
-  def unique_name
+class KlassMethod < ActiveRecord::Base
+  include IsProcedure
+  belongs_to :klass, inverse_of: :klass_methods
+  def derived_unique_name
     "#{klass.unique_name}#{language.namespace_separator}#{name}"
   end
 end
 
-class Function
-  include IsAProcedure
-  belongs_to :namespace, class_name: 'Namespace', inverse_of: :functions
-  def unique_name
+class Function < ActiveRecord::Base
+  include IsProcedure
+  belongs_to :namespace, inverse_of: :functions
+  def derived_unique_name
     "#{namespace.unique_name}#{language.namespace_separator}#{name}"
   end
 end
 
-class GlobalVariable < Variable
+class GlobalVariable < ActiveRecord::Base
+  include IsVariable
   field :type, type: String, default: 'GLOBALS'
   belongs_to :global_scope, polymorphic: true
   after_initialize do
     self.global_scope ||= Namespace.find_or_create_by(language.global_namespace)
   end
-  def unique_name
+  def derived_unique_name
     "#{language.global_namespace['unique_name']}#{language.namespace_separator}#{type}[#{name}]"
   end
 end
 
-class LocalVariable < Variable
+class LocalVariable < ActiveRecord::Base
+  include IsVariable
   belongs_to :local_scope, polymorphic: true
-  def unique_name
+  def derived_unique_name
     "#{local_scope.unique_name}#{language.namespace_separator}#{name}"
   end
 end
 
-class Property < Variable
+class Property < ActiveRecord::Base
+  include IsVariable
   field :type, type: String
-  belongs_to :klass, class_name: 'Klass', inverse_of: :properties
+  belongs_to :klass
   scope :instances_properties, ->{ where(type: language.instance_property) }
-  def unique_name
+  def derived_unique_name
     "#{klass.unique_name}#{language.namespace_separator}#{name}"
   end
 end
 
-class Parameter < Variable
+class Parameter < ActiveRecord::Base
+  include IsVariable
   belongs_to :procedure, polymorphic: true
-  def unique_name
+  def derived_unique_name
     "#{procedure.unique_name}#{language.namespace_separator}#{name}"
   end
 end
 
-class Assignement
-  include ReferencesLanguage
-  include HasAName
-  field :name, type: String, overwrite: true, default: ->{ variable.unique_name }
+class Assignement < ActiveRecord::Base
+  include HasNameAndUniqueName
   field :position, type: Array
-  field :rhs, type: String
+  field :rhs, type: xml
   belongs_to :variable, polymorphic: true
-  def unique_name
+  def derived_unique_name
     "#{variable.unique_name}#{language.namespace_separator}#{position}"
   end
 end
