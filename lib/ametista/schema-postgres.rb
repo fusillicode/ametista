19c2f28fa04588d0c9c1b702faea974c3307db5b
fusillicode <fusillicode@gmail.com>
2014-12-15 11:42:36 +0100

    create database...yeah...

--- lib/ametista/schema-postgres.rb
+++ lib/ametista/schema-postgres.rb
@@ -1,40 +1,66 @@
 ActiveRecord::Schema.define do
 
-  create_table 'types', :force => true do |t|
+  create_table 'klasses', :force => true do |t|
     t.string :name, null: false
-    t.belongs_to :namespace, class_name: 'Namespace', inverse_of: :klasses
-    t.belongs_to :parent_klass, class_name: 'Klass', inverse_of: :child_klasses
-    t.has_many :child_klasses, class_name: 'Klass', inverse_of: :parent_klass
-    t.has_many :methods, class_name: 'KlassMethod', inverse_of: :klass
-    t.has_many :properties, class_name: 'Property', inverse_of: :klass
-    t.string  :global_scope_type
+    t.belongs_to :namespace
+    t.references :parent_klass
   end
 
-  create_table 'variables_types', id: false, :force => true do |t|
+  create_table 'primitive_types', :force => true do |t|
     t.string :name, null: false
-    t.integer :variable_id
-    t.integer :type_id
   end
 
-  create_table 'variables', :force => true do |t|
-    t.string  :name, null: false
-    belongs_to :procedure, polymorphic: true
+  create_table 'variable_types', :force => true do |t|
+    t.belongs_to :variable
+    t.belongs_to :type
+  end
+
+  create_table 'global_variables', :force => true do |t|
+    t.string :name, null: false
+    t.string :type
+    t.references :global_scope, polymorphic: true
+  end
+
+  create_table 'local_variables', :force => true do |t|
+    t.string :name, null: false
+    t.string :type
+    t.references :local_scope, polymorphic: true
+  end
+
+  create_table 'properties', :force => true do |t|
+    t.string :name, null: false
+    t.string :type
+    t.belongs_to :klass
+  end
+
+  create_table 'parameters', :force => true do |t|
+    t.string :name, null: false
+    t.string :type
+    t.references :procedure, polymorphic: true
   end
 
   create_table 'namespaces', :force => true do |t|
-    t.string :name, null: false
+    t.string :unique_name, null: false
+    t.column :statements, :xml
   end
 
-  create_table 'procedures', :force => true do |t|
+  create_table 'functions', :force => true do |t|
     t.string :name, null: false
+    t.belongs_to :namespace
+    t.column :statements, :xml
+  end
+
+  create_table 'klass_methods', :force => true do |t|
+    t.string :name, null: false
+    t.belongs_to :klass
+    t.column :statements, :xml
   end
 
   create_table 'assignements', :force => true do |t|
     t.string :name, null: false
     t.string :position, array: true
     t.column :rhs, :xml
-    t.integer :variable_id
-    t.string  :variable_type
+    t.references :variable, polymorphic: true
   end
 
 end
