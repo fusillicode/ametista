f44d1edaead6339a211c906d5e6ae4a44dd35a76
fusillicode <fusillicode@gmail.com>
2014-12-16 17:30:25 +0100

    remove jsoinfier and forwardable

--- lib/ametista/builders/builder.rb
+++ lib/ametista/builders/builder.rb
@@ -1,14 +1,9 @@
-require 'forwardable'
 require_relative '../queriers/querier'
-require_relative '../jsonifier'
 
 class Builder
   extend Initializer
-  extend Forwardable
   initialize_with ({
-    querier: Querier.new,
-    jsonifier: JSONifier.new,
+    querier: Querier.new
     ast: nil
   })
-  def_delegator :@jsonifier, :jsonify, :jsonify
 end
