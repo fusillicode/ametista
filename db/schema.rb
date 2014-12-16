e3a8b3cc528ad6f4c760f2b93d135f7630a9a9a0
fusillicode <fusillicode@gmail.com>
2014-12-16 16:53:53 +0100

    postgres schema

--- db/schema.rb
+++ db/schema.rb
@@ -1,13 +1,10 @@
 ActiveRecord::Schema.define do
 
-  create_table 'klasses', :force => true do |t|
+  create_table 'types', :force => true do |t|
     t.string :name, null: false
+    t.string :unique_name
     t.belongs_to :namespace
     t.references :parent_klass
-  end
-
-  create_table 'primitive_types', :force => true do |t|
-    t.string :name, null: false
   end
 
   create_table 'variable_types', :force => true do |t|
@@ -15,51 +12,39 @@ ActiveRecord::Schema.define do
     t.belongs_to :type
   end
 
-  create_table 'global_variables', :force => true do |t|
+  create_table 'variables', :force => true do |t|
     t.string :name, null: false
+    t.string :unique_name
     t.string :type
-    t.references :global_scope, polymorphic: true
-  end
-
-  create_table 'local_variables', :force => true do |t|
-    t.string :name, null: false
-    t.string :type
-    t.references :local_scope, polymorphic: true
-  end
-
-  create_table 'properties', :force => true do |t|
-    t.string :name, null: false
-    t.string :type
+    t.references :scope, polymorphic: true
     t.belongs_to :klass
-  end
-
-  create_table 'parameters', :force => true do |t|
-    t.string :name, null: false
-    t.string :type
-    t.references :procedure, polymorphic: true
+    t.belongs_to :procedure
   end
 
   create_table 'namespaces', :force => true do |t|
     t.string :unique_name, null: false
-    t.column :statements, :xml
+    t.xml :statements
   end
 
   create_table 'functions', :force => true do |t|
     t.string :name, null: false
+    t.string :unique_name
     t.belongs_to :namespace
-    t.column :statements, :xml
+    t.xml :statements
   end
 
   create_table 'klass_methods', :force => true do |t|
     t.string :name, null: false
+    t.string :unique_name
     t.belongs_to :klass
-    t.column :statements, :xml
+    t.xml :statements
   end
 
   create_table 'assignements', :force => true do |t|
     t.string :name, null: false
+    t.string :unique_name, null: false
     t.string :position, array: true
-    t.column :rhs, :xml
+    t.xml :rhs
     t.references :variable, polymorphic: true
   end
 
