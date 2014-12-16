b13a425e88bc4e58a9dd2898061f76f643fce10c
fusillicode <fusillicode@gmail.com>
2014-12-16 22:13:46 +0100

    model builder handle the connection to the db

--- bin/build.rb
+++ bin/build.rb
@@ -2,9 +2,5 @@ if __FILE__ == $0
   require 'active_record'
   require 'yaml'
   require_relative '../lib/ametista/builders/model_builder'
-  config_file = File.join Dir.pwd, 'db', 'config.yml'
-  ENV['DB'] = 'development'
-  config = YAML::load(File.open config_file)
-  ActiveRecord::Base.establish_connection config[ENV['DB']]
   model_builder = ModelBuilder.new.build
 end
