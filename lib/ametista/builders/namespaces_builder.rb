1b4463f16e41fcc5e82766173e796e677a66784f
fusillicode <fusillicode@gmail.com>
2014-12-18 15:14:12 +0100

    return array of namespaces objects

--- lib/ametista/builders/namespaces_builder.rb
+++ lib/ametista/builders/namespaces_builder.rb
@@ -18,7 +18,8 @@ class NamespacesBuilder < Builder
   def global_namespace
     namespace = Namespace.find_or_create_by(
       unique_name: querier.global_namespace_unique_name
-    ).contents << content(querier.global_namespace_statements(ast))
+    )
+    namespace.contents << content(querier.global_namespace_statements(ast))
     namespace
   end
 
@@ -37,7 +38,8 @@ class NamespacesBuilder < Builder
   def namespace namespace_ast
     namespace = Namespace.find_or_create_by(
       unique_name: querier.unique_name(namespace_ast),
-    ).contents << content(querier.statements(namespace_ast))
+    )
+    namespace.contents << content(querier.statements(namespace_ast))
     namespace
   end
 
