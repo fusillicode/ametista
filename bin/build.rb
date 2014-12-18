eac37906699068918ed222ef5dcb88fef1bba146
fusillicode <fusillicode@gmail.com>
2014-12-18 14:51:34 +0100

    add db cleanup on startup

--- bin/build.rb
+++ bin/build.rb
@@ -5,5 +5,7 @@ if __FILE__ == $0
   Global.environment = 'development'
   Global.config_directory = File.join(Dir.pwd, 'config')
   ActiveRecord::Base.establish_connection Global.db.to_hash
+  # Per pulire il db
+  `rake db:reset`
   model_builder = ModelBuilder.new.build
 end
