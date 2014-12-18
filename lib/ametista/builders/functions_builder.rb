817a470bb3abc99b7906b3a8f364be03a6a24e0b
fusillicode <fusillicode@gmail.com>
2014-12-18 15:16:59 +0100

    add contents for functions

--- lib/ametista/builders/functions_builder.rb
+++ lib/ametista/builders/functions_builder.rb
@@ -16,19 +16,26 @@ class FunctionsBuilder < Builder
   end
 
   def functions
-    querier.functions(ast).map_unique('_id') do |function_ast|
+    querier.functions(ast).map_unique('id') do |function_ast|
       function(function_ast)
     end
   end
 
   def function function_ast
-    Function.find_or_create_by(
+    function = Function.find_or_create_by(
       name: querier.name(function_ast),
       namespace: namespace(
         querier.function_namespaced_name_parts(function_ast)
-      ),
-      statements: querier.statements(function_ast)
+      )
     )
+    function.contents << content(querier.statements(function_ast))
+    function
+  end
+
+  # TODO rimuovere il check sui statements quando Postgres consentirà l'inserimento
+  # di stringhe vuote per il campo xml
+  def content statements
+    Content.new({ statements: (statements.empty? ? ' ' : statements) })
   end
 
   def namespace name_parts
