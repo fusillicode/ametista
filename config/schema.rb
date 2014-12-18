565e2beb557c6aa485bf851e8a68480eacdf5b66
fusillicode <fusillicode@gmail.com>
2014-12-18 11:49:54 +0100

    add database schema

---
+++ config/schema.rb
@@ -0,0 +1,53 @@
+ActiveRecord::Schema.define(:version => 0) do
+
+  create_table "assignements", :force => true do |t|
+    t.string :name, :null => false
+    t.string :unique_name
+    t.string :position
+    t.xml :rhs
+    t.references :variable, polymorphic: true
+  end
+
+  create_table "contents", :force => true do |t|
+    t.xml :statements
+    t.references :container, polymorphic: true
+  end
+
+  create_table "functions", :force => true do |t|
+    t.string :name, :null => false
+    t.string :unique_name
+    t.integer :namespace_id
+  end
+
+  create_table "klass_methods", :force => true do |t|
+    t.string :name, :null => false
+    t.string :unique_name
+    t.integer :klass_id
+  end
+
+  create_table "namespaces", :force => true do |t|
+    t.string :unique_name, :null => false
+  end
+
+  create_table "types", :force => true do |t|
+    t.string :name, :null => false
+    t.string :unique_name
+    t.integer :namespace_id
+    t.references :parent_klass
+  end
+
+  create_table "variable_types", :force => true do |t|
+    t.integer :variable_id
+    t.integer :type_id
+  end
+
+  create_table "variables", :force => true do |t|
+    t.string :name, :null => false
+    t.string :unique_name
+    t.string :type
+    t.references :scope, polymorphic: true
+    t.integer :klass_id
+    t.integer :procedure_id
+  end
+
+end
