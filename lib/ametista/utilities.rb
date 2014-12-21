445c53f5ee26357ad707e1b1d8facbb158742b57
fusillicode <fusillicode@gmail.com>
2014-12-21 08:26:21 +0100

    remove old utilities for Mongoid

--- lib/ametista/utilities.rb
+++ lib/ametista/utilities.rb
@@ -25,16 +25,6 @@ module Initializer
   end
 end
 
-# module PatchedAwesomePrint
-#   require 'awesome_print'
-#   ::Moped::BSON = ::BSON
-# end
-
-module EnforceAvailableLocales
-  # per fixare "[deprecated] I18n.enforce_available_locales will default to true in the future. If you really want to skip validation of your locale you can set I18n.enforce_available_locales = false to avoid this message."
-  I18n.config.enforce_available_locales = false
-end
-
 module UniqueMapper
   def map_unique unique_id, &block
     self.map(&block).compact.uniq{ |x| x.send("#{unique_id}") }
