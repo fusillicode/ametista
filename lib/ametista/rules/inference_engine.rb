e62659970b5acc5fbfe35562a43b206085ac99af
fusillicode <fusillicode@gmail.com>
2015-01-08 20:40:16 +0100

    initial inference engine iteration

--- lib/ametista/rules/inference_engine.rb
+++ lib/ametista/rules/inference_engine.rb
@@ -9,12 +9,35 @@ class InferenceEngine
   initialize_with ({
     rules: {
       methods_calls_rule: MethodsCallsRule.new
-    }
+    },
+    model_modified: false,
+    total_iterations: 7,
+    current_iteration: 1
   })
 
+  alias_method :model_modified?, :model_modified
+
   def infer
-    rules.each do |key, rule|
+    apply_rules while continue?
+  end
+
+  def continue?
+    model_modified? and current_iteration <= total_iterations
+  end
+
+  def apply_rules
+    current_iteration += 1
+    model_modified = rules_application_result
+  end
+
+  def rules_application_result
+    rules_application.reduce :&
+  end
+
+  def rules_application
+    rules.map do |key, rule|
       rule.apply
     end
   end
+
 end
