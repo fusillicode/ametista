720557964cbf0493fadc74af845f6d9ca9363759
fusillicode <fusillicode@gmail.com>
2015-01-13 12:58:49 +0100

    comments on querier

--- lib/ametista/queriers/querier.rb
+++ lib/ametista/queriers/querier.rb
@@ -8,6 +8,8 @@ class Querier
     language_querier: LanguageQuerier.new
   })
 
+  # TODO, se la situazione del Querier rimane questa, ovvero quella di un oggetto che forwarda al LanguageQuerier
+  # si può pensare di usare Forwardable
   def method_missing method_name, *args, &block
     if self.respond_to? method_name
       self.public_send method_name, *args, &block
