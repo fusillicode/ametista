6e97a95a1ece79a1b29b598d1422dae40e5ae7a4
fusillicode <fusillicode@gmail.com>
2015-02-27 12:53:37 +0100

    ...

--- lib/ametista/rules/global_variables_uses_rule.rb
+++ lib/ametista/rules/global_variables_uses_rule.rb
@@ -28,19 +28,35 @@ class GlobalVariablesUsesRule < RulesCollection
 
   def types_for_global_variables_uses content
     GlobalVariable.find_each do |global_variable|
-      ap types_for_klass_methods_calls(content, global_variable.name) | global_variable.types
+      types_for_klass_methods_calls content, global_variable.name
     end
   end
 
-  def types_for_klass_methods_calls content, name
-    querier.klass_methods_calls(content, name).map do |klass_method_call|
-      types klass_method_call
-    end.flatten
+  # def types_for_global_variable_uses content, global_variable
+  #   inferred_types = types_for_klass_methods_calls content, global_variable.name
+  #   current_types = global_variable.types
+  #   return inferred_types if current_types.empty?
+  #   intersection_of_types = current_types & inferred_types
+  #   return intersection_of_types if intersection_of_types.empty?
+  #   global_variable.types
+  #     types_for_klass_methods_calls(content, global_variable.name) | global_variable.types
+  #   end
+  # end
+
+  def types_for_klass_methods_calls content, global_variable
+    querier.klass_methods_calls(content, global_variable.name).map do |klass_method_call|
+      inferred_types = types_for_klass_method_call klass_method_call
+      current_types = global_variable.types
+      return inferred_types if current_types.empty?
+      intersection_of_types = current_types & inferred_types
+      return intersection_of_types if not(intersection_of_types.empty?)
+      return current_types & inferred_types
+    end
   end
 
-  def types klass_method_call
+  def types_for_klass_method_call klass_method_call
     KlassMethod.where(name: querier.name(klass_method_call)).map do |klass_method|
-      klass_method.klass
+      klass_method.klass.descendants
     end
   end
 
