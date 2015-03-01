c44bb12d7479ba720d2adadc174f16334e11f665
fusillicode <fusillicode@gmail.com>
2015-03-01 16:49:15 +0100

    messing with method missing on rule collections but it seems to work

--- lib/ametista/rules/rules_collection.rb
+++ lib/ametista/rules/rules_collection.rb
@@ -2,51 +2,49 @@ require_relative 'rule'
 
 class RulesCollection < Rule
 
-  attribute :default_rule, Rule
   attribute :override_rules, Axiom::Types::Boolean, default: false
-  attribute :rules, Hash, default: {
-    'Expr_AssignOp_ShiftLeft'      => 'int',
-    'Expr_AssignOp_ShiftRight'     => 'int',
-    'Expr_Array'                   => 'array',
-    'Expr_BinaryOp_BooleanAnd'     => 'bool',
-    'Expr_BinaryOp_BooleanOr'      => 'bool',
-    'Expr_BinaryOp_Concat'         => 'string',
-    'Expr_BinaryOp_Equal'          => 'bool',
-    'Expr_BinaryOp_Greater'        => 'bool',
-    'Expr_BinaryOp_GreaterOrEqual' => 'bool',
-    'Expr_BinaryOp_Identical'      => 'bool',
-    'Expr_BinaryOp_LogicalAnd'     => 'bool',
-    'Expr_BinaryOp_LogicalOr'      => 'bool',
-    'Expr_BinaryOp_LogicalXor'     => 'bool',
-    'Expr_BinaryOp_NotIdentical'   => 'bool',
-    'Expr_BinaryOp_Smaller'        => 'bool',
-    'Expr_BinaryOp_SmallerOrEqual' => 'bool',
-    'Expr_BinaryOp_ShiftLeft'      => 'int',
-    'Expr_BinaryOp_ShiftRight'     => 'int',
-    'Expr_Closure'                 => 'callback',
-    'Expr_Cast_Array'              => 'array',
-    'Expr_Cast_Bool'               => 'bool',
-    'Expr_Cast_Double'             => 'float',
-    'Expr_Cast_Int'                => 'int',
-    'Expr_Cast_Object'             => 'object',
-    'Expr_Cast_String'             => 'string',
-    'Expr_Cast_Unset'              => 'NULL',
-    'Expr_InstanceOf'              => 'bool',
-    'Scalar_DNumber'               => 'float',
-    'Scalar_LNumber'               => 'int'
-  }
+  attribute :rules, Hash, default: {}
 
   def initialize args = {}, &block
     super
-    @default_rule = args[:default_rule] || Rule.new
-    init_rules
+    add_rules ({
+          'Expr_AssignOp_ShiftLeft'      => 'int',
+          'Expr_AssignOp_ShiftRight'     => 'int',
+          'Expr_Array'                   => 'array',
+          'Expr_BinaryOp_BooleanAnd'     => 'bool',
+          'Expr_BinaryOp_BooleanOr'      => 'bool',
+          'Expr_BinaryOp_Concat'         => 'string',
+          'Expr_BinaryOp_Equal'          => 'bool',
+          'Expr_BinaryOp_Greater'        => 'bool',
+          'Expr_BinaryOp_GreaterOrEqual' => 'bool',
+          'Expr_BinaryOp_Identical'      => 'bool',
+          'Expr_BinaryOp_LogicalAnd'     => 'bool',
+          'Expr_BinaryOp_LogicalOr'      => 'bool',
+          'Expr_BinaryOp_LogicalXor'     => 'bool',
+          'Expr_BinaryOp_NotIdentical'   => 'bool',
+          'Expr_BinaryOp_Smaller'        => 'bool',
+          'Expr_BinaryOp_SmallerOrEqual' => 'bool',
+          'Expr_BinaryOp_ShiftLeft'      => 'int',
+          'Expr_BinaryOp_ShiftRight'     => 'int',
+          'Expr_Closure'                 => 'callback',
+          'Expr_Cast_Array'              => 'array',
+          'Expr_Cast_Bool'               => 'bool',
+          'Expr_Cast_Double'             => 'float',
+          'Expr_Cast_Int'                => 'int',
+          'Expr_Cast_Object'             => 'object',
+          'Expr_Cast_String'             => 'string',
+          'Expr_Cast_Unset'              => 'NULL',
+          'Expr_InstanceOf'              => 'bool',
+          'Scalar_DNumber'               => 'float',
+          'Scalar_LNumber'               => 'int'
+        })
+    self.default = args[:default_rule] || Rule.new
   end
 
-  def init_rules
-    @rules = rules.each_with_object({}) do |(name, logic), hash|
-      hash[name] = Rule.new name: name, logic: logic
+  def add_rules rules = {}
+    rules.map do |key, rule|
+      add_rule key, rule
     end
-    @rules.default = default_rule
   end
 
   def apply *args
@@ -59,27 +57,29 @@ class RulesCollection < Rule
     @rules[name].apply *args
   end
 
-  def add_rule args = {}, &block
-    key = args[:key] || args[:name]
-    if not(override_rules) && @rules.has_key? key
+  def add_rule key = nil, rule = {}, &block
+    key = key || rule[:name]
+    if not(override_rules) && @rules.has_key?(key)
       raise "There is already a rule identified by the key '#{key}'"
     end
-    @rules[key] = Rule.new args, &block
+    name = rule[:name] rescue key
+    logic = rule[:logic] rescue rule
+    @rules[key] = rule.is_a?(Rule) ? rule : Rule.new(name: name, logic: logic, &block)
+  end
+
+  def []= key, value
+    add_rule key: key, value: value
   end
 
   def remove_rule name
     @rules.delete_key name
   end
 
-  def []= key, value
-    add_rule key: key, logic: value
-  end
-
   def method_missing method_name, *args, &block
     if self.respond_to? method_name
       self.public_send method_name, *args, &block
     else
-      rules.public_send method_name, *args, &block
+      @rules.public_send method_name, *args, &block
     end
   end
 
